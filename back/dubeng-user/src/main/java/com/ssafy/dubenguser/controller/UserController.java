b89bee4929b2dbf480ec498d97cf5288dbd6add6
lancelot1672 <lancelot1672@naver.com>
2023-04-27 10:58:12 +0900

    add join, duplicate check nickname

--- back/dubeng-user/src/main/java/com/ssafy/dubenguser/controller/UserController.java
+++ back/dubeng-user/src/main/java/com/ssafy/dubenguser/controller/UserController.java
@@ -1,37 +1,89 @@
 package com.ssafy.dubenguser.controller;
 
+import com.ssafy.dubenguser.dto.TokenDTO;
+import com.ssafy.dubenguser.dto.UserJoinRequestDTO;
+import com.ssafy.dubenguser.service.AuthService;
 import com.ssafy.dubenguser.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @Slf4j
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final AuthService authService;
 
     @GetMapping("/kakao/callback")
-    public void getAuthCode(@RequestParam String code, HttpServletResponse response) throws IOException {
+    public void getAuthCode(@RequestParam String code, HttpServletResponse response, RedirectAttributes attributes) throws IOException {
         log.debug("auth code : {}", code);
 
         //code로 access-token 요청
-        String redirectUri = userService.getAccessToken(code);
+        HashMap<String, Object> result = authService.getAccessToken(code);
 
+        //회원 가입 여부 체크
+        String redirectUri = "/";
+        if(!userService.checkEnrolledMember((Long) result.get("userId"))){
+            redirectUri = "/join";
+        }
+
+        //토큰 POST 방식 적재
+        attributes.addFlashAttribute("token", result);
         response.sendRedirect("http://localhost:3000" + redirectUri);
     }
 
-    @PostMapping("/parse/{token}")
-    public ResponseEntity<String> parseAccessToken(@RequestBody String token){
-        log.debug("accessToken : {}", token);
+    @PostMapping("/parse")
+    public ResponseEntity<Long> parseAccessToken(@RequestBody TokenDTO requestDTO){
+        log.debug("accessToken : {}", requestDTO.getAccessToken());
+
+        //service - parseToken
+        Long userId = authService.parseToken(requestDTO.getAccessToken());
+
+        return new ResponseEntity<Long>(userId, HttpStatus.OK);
+    }
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenDTO> refreshToken(@RequestBody TokenDTO requestDTO){
+        log.debug("refreshToken : {}", requestDTO);
+
+        //service - refresh
+        TokenDTO responseDTO = authService.requestRefresh(requestDTO);
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    /**
+     * 회원가입
+     * 구현 안돼어있음.
+     */
+    @PostMapping("join")
+    public void join(@RequestBody UserJoinRequestDTO requestDTO){
+        log.debug("requestDTO : {}", requestDTO.toString());
+
+        Long userId = authService.parseToken(requestDTO.getAccessToken());
+
+        if(userService.checkEnrolledMember(userId)){
+            //이미 등록된 사용자 입니다.
+        }
+
+        userService.save(requestDTO);
 
 
-        return new ResponseEntity<String>("", HttpStatus.OK);
+    }
+    @GetMapping("/check/{nickname}")
+    public ResponseEntity<Boolean> checkDuplicateNickname(@PathVariable String nickname){
+        log.debug("nickname : {}", nickname);
+
+        boolean check = userService.isExistNickname(nickname);
+
+        return new ResponseEntity<Boolean>(check, HttpStatus.OK);
     }
 }
--- back/dubeng-user/src/main/java/com/ssafy/dubenguser/repository/UserRepository.java
+++ back/dubeng-user/src/main/java/com/ssafy/dubenguser/repository/UserRepository.java
@@ -4,5 +4,8 @@ package com.ssafy.dubenguser.repository;
 import com.ssafy.dubenguser.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByNickname(String nickname);
 }
