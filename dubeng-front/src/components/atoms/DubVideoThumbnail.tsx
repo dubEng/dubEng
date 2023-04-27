14447e5962f04c840f09cd6ad6157933b6579e86
최정온 <chjo0330@gmail.com>
2023-04-27 23:40:05 +0900

    feat: DubVideoThumbnail Component and StoryBook

--- dubeng-front/src/components/atoms/DubVideoThumbnail.tsx
+++ dubeng-front/src/components/atoms/DubVideoThumbnail.tsx
@@ -1,3 +1,17 @@
-export default function DubVideoThumbnail() {
-  return <></>;
+import Image from "next/image";
+import WeBareBears from "../../../public/images/dump/webarebears_image.png"
+
+
+interface Iprops {
+  title: string;
+  url: string;
+}
+
+export default function DubVideoThumbnail({ title = "제목입니다.", url="" }: Iprops) {
+  return (
+    <div className="relative object-cover">
+    <Image src={WeBareBears} className="rounded-18" alt={title} width={272} height={152} />
+    <p className="absolute bottom-16 left-16 text-white font-semibold text-12">{title}</p>
+    </div>
+  );
 }
