82d2aac30247146a3444385ff6fa941f39db21e9
lancelot1672 <lancelot1672@naver.com>
2023-04-28 17:35:41 +0900

    add RedisConnection and Filepath save in Cache

--- back/storage/src/main/java/com/ssafy/storage/controller/FileController.java
+++ back/storage/src/main/java/com/ssafy/storage/controller/FileController.java
@@ -1,43 +1,36 @@
 package com.ssafy.storage.controller;
 
 import com.ssafy.storage.dto.SaveFileRequestDTO;
+import com.ssafy.storage.service.SaveFileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RestController
 @RequestMapping("/dub")
 @RequiredArgsConstructor
 public class FileController {
+    private final SaveFileService fileService;
 
     @PostMapping("/upload")
     public void fileUpload(@ModelAttribute SaveFileRequestDTO requestDTO) throws IOException {
         log.debug(requestDTO.toString());
 
-        String fullPath = "";
-        String os = System.getProperty("os.name").toLowerCase();
-
-        log.debug("os type : {}", os);
-        if(os.contains("win")){
-            fullPath = "c:/Home/";
-        }else if(os.contains("linux")){
-            fullPath = "/Home/";
-        }
-        if(!requestDTO.getAudioFile().isEmpty()){
-            //파일이 있어야
-
-            fullPath += requestDTO.getAudioFile().getOriginalFilename();
-            log.debug("file full path : {}",fullPath);
-            requestDTO.getAudioFile().transferTo(new File(fullPath));
-        }
-
+        fileService.fileSave(requestDTO);
     }
+    @GetMapping("/get/file")
+    public Set<Object> getfilePath(@RequestParam String videoId, @RequestParam String userId){
+        log.debug("videoId : {}, userId : {}", videoId, userId);
 
+        String key = videoId + "_" + userId;
+        Set<Object> pathList = fileService.getPathInCache(key);
+
+        return pathList;
+    }
 }
