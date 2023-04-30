a560f1b0b81f5100ad1c46fe7436d40165448a88
최정온 <chjo0330@gmail.com>
2023-04-30 18:27:01 +0900

    feat: HomePage Update

--- dubeng-front/src/pages/index.tsx
+++ dubeng-front/src/pages/index.tsx
@@ -1,5 +1,28 @@
-import Background from "@/components/atoms/Background";
+import UserRankingList from "@/features/home/organism/UserRankingList";
+import DubKingList from "@/features/home/organism/DubKingList";
+import Banner from "@/features/home/atoms/Banner";
+import DubProductList from "@/features/home/organism/DubProductList";
 
 export default function HomePage() {
-  return <>홈 페이지</>;
+  return (
+    <>
+      <div className="m-16">
+        <Banner />
+      </div>
+      <p className="flex justify-start mx-16 text-19 font-bold mt-24 mb-16">
+        실시간 인기 더빙
+      </p>
+      <div className="ml-16 ">
+        <DubProductList />
+      </div>
+      <p className="flex justify-start mx-16 text-19 font-bold mt-24">
+        유저 랭킹
+      </p>
+      <UserRankingList />
+      <p className="flex justify-start mx-16 text-19 font-bold mt-24">
+        이주의 더빙왕
+      </p>
+      <DubKingList />
+    </>
+  );
 }
