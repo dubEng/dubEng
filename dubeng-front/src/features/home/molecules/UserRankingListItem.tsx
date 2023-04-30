a9dbef90334c79f4daef4ac7720d6c4137460b5c
최정온 <chjo0330@gmail.com>
2023-04-30 15:57:54 +0900

    feat: edit UserRankingListItem

--- dubeng-front/src/features/home/molecules/UserRankingListItem.tsx
+++ dubeng-front/src/features/home/molecules/UserRankingListItem.tsx
@@ -1,13 +1,5 @@
+import { UserRanking } from "@/types/UserRanking";
 import Image from "next/image";
-
-interface Iprops {
-  ranking: number;
-  imageUrl: string;
-  nickname: string;
-  introduce: string;
-  recordingTime: number;
-  dubingCount: number;
-}
 
 export default function UserRankingListItem({
   ranking,
@@ -16,23 +8,23 @@ export default function UserRankingListItem({
   introduce,
   recordingTime,
   dubingCount,
-}: Iprops) {
+}: UserRanking) {
   return (
-    <div className="grid grid-cols-12 h-38 gap-8 ">
+    <div className="grid grid-cols-12 h-38 gap-8 mb-16">
       <div className="text-14 text-dubblack col-span-2 flex items-center justify-center">
         {ranking}
       </div>
       <div className="flex justify-end">
-        <Image src={imageUrl} alt={"profile"} width={38} height={38} />
+        <Image src={imageUrl} alt={"profile"} width={38} height={38} className="rounded-md" />
       </div>
       <div className="col-span-7">
         <div className="text-14 text-dubblack">{nickname}</div>
         <div className="text-10 text-dubgray">{introduce}</div>
       </div>
-      <div className="text-14 text-dubblack flex items-center">
+      <div className="text-14 text-dubblack flex items-center justify-center">
         {recordingTime}
       </div>
-      <div className="text-14 text-dubblack flex items-center">
+      <div className="text-14 text-dubblack flex items-center justify-center">
         {dubingCount}
       </div>
     </div>
