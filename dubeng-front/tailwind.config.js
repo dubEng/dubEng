8c34e11686e7aaf542ef489f6359f13022762d71
최정온 <chjo0330@gmail.com>
2023-04-30 15:05:24 +0900

    fix: edit tailwind config

--- dubeng-front/tailwind.config.js
+++ dubeng-front/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
