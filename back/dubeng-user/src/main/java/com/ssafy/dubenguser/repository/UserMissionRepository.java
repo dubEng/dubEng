fad2383bce472235780221b2323d5149223b6c61
김지희 <cjg05034@naver.com>
2023-05-01 17:14:32 +0900

    feat: add APIs that search user missions and mission assets completed

---
+++ back/dubeng-user/src/main/java/com/ssafy/dubenguser/repository/UserMissionRepository.java
@@ -0,0 +1,24 @@
+package com.ssafy.dubenguser.repository;
+
+import com.ssafy.dubenguser.dto.UserMissionRes;
+import com.ssafy.dubenguser.entity.User;
+import com.ssafy.dubenguser.entity.UserMission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
+
+    @Query("select new com.ssafy.dubenguser.dto.UserMissionRes(m.video.id, m.title, m.assets, m.color, um.isComplete)" +
+            " from UserMission um " +
+            "left join Mission m on m.id = um.mission.id" +
+            " where um.user=:user")
+    public List<UserMissionRes> findUserMissionsByUser(User user);
+
+    @Query("select m.assets" +
+            " from UserMission um " +
+            "left join Mission m on m.id = um.mission.id" +
+            " where um.user=:user and um.isComplete=true")
+    public List<String> findAssetsByUser(User user);
+}
