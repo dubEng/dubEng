49ee1134e82db968a29baf28f7a4a50e630fd95f
최정온 <chjo0330@gmail.com>
2023-05-01 17:04:03 +0900

    feat: add MissionPage

--- dubeng-front/src/pages/mission/index.tsx
+++ dubeng-front/src/pages/mission/index.tsx
@@ -1,3 +1,15 @@
-export default function MissionPage(){
-  return (<>도전과제</>);
+import MissionList from "@/features/mission/organism/MissionList";
+
+export default function MissionPage() {
+  return (
+    <div className="container mx-auto">
+      <p className="flex justify-start mx-16 text-19 font-bold mt-24 mb-16">
+        나의 섬
+      </p>
+      <p className="flex justify-start mx-16 text-19 font-bold mt-24 mb-16">
+        도전과제
+      </p>
+      <MissionList />
+    </div>
+  );
 }
