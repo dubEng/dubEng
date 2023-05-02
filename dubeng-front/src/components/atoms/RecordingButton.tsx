57f42c3152501a45de88db48fe8f7147210623a8
최정온 <chjo0330@gmail.com>
2023-05-02 13:12:52 +0900

    feat: add DubVideoSlider

--- dubeng-front/src/components/atoms/RecordingButton.tsx
+++ dubeng-front/src/components/atoms/RecordingButton.tsx
@@ -2,17 +2,31 @@ import Image from "next/image";
 import defaultRecordingIcon from "../../../public/icons/recording-icon/defaultRecordingIcon.svg";
 import dubbingRecordingIcon from "../../../public/icons/recording-icon/dubbingRecordingIcon.svg";
 import shortsRecordingIcon from "../../../public/icons/recording-icon/shortsRecordingIcon.svg";
+import Sheet from "react-modal-sheet";
+import { useState } from "react";
+import DubVideoSlider from "../organism/DubVideoSlider";
 
 interface Iprops {
   page: string;
 }
 
 export default function RecordingButton({ page }: Iprops) {
+  const [isOpen, setOpen] = useState(false);
+
   if (page === "/dubbing") {
     return <Image src={dubbingRecordingIcon} alt="dubbingRecordingIcon" />;
   } else if (page === "/community/shorts") {
     return <Image src={shortsRecordingIcon} alt="shortsRecordingIcon" />;
   } else {
-    return <Image src={defaultRecordingIcon} alt="defaultRecordingIcon" />;
+    return (
+      <>
+        <Image
+          src={defaultRecordingIcon}
+          alt="defaultRecordingIcon"
+          onClick={() => setOpen(true)}
+        />
+        <DubVideoSlider isOpen={isOpen} setOpen={setOpen} />
+      </>
+    );
   }
 }
--- dubeng-front/src/components/organism/DubVideoSlider.tsx
+++ dubeng-front/src/components/organism/DubVideoSlider.tsx
@@ -1,3 +1,22 @@
-export default function DubVideoSlider() {
-  return <></>;
+import Sheet from 'react-modal-sheet';
+
+interface Iprops{
+  isOpen: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+
+export default function DubVideoSlider({isOpen, setOpen}: Iprops) {
+  return (
+    <>
+      <button onClick={() => setOpen(true)}>Open sheet</button>
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{/* Your sheet content goes here */}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    </>
+  );
 }
