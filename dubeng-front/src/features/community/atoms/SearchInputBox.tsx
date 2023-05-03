c5c90fbd0901ed771d2ebb41c2e2452fd384d16f
김도언 <vov06144@naver.com>
2023-05-04 06:14:59 +0900

    feat: add SearchInputBox

--- dubeng-front/src/features/community/atoms/SearchInputBox.tsx
+++ dubeng-front/src/features/community/atoms/SearchInputBox.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import searchIcon from "../../../../public/icons/search-icon/search 1.svg";
+import { AiFillCloseCircle } from "react-icons/ai";
 interface Iprops {
   type: string;
   name: string;
@@ -15,15 +18,47 @@ export default function SearchInputBox({
   onKeyDown,
   placeholder = "더빙할 콘텐츠를 검색해보세요.",
 }: Iprops) {
-  return (
-    <input
-      className="text-16 rounded-5 font-normal placeholder-dubgray text-dubblack outline-none h-43 w-358 border border-[#E9ECEF] pl-16 py-12"
-      type={type}
-      name={name}
-      onChange={onChange}
-      value={value}
-      placeholder={placeholder}
-      onKeyDown={onKeyDown}
-    ></input>
-  );
+  if (value) {
+    return (
+      <div className="flex relative items-center w-358 h-43">
+        <input
+          className="text-16 rounded-5 font-normal placeholder-dubgray text-dubblack outline-none h-43 w-358 border border-[#E9ECEF] pl-35 py-12"
+          type={type}
+          name={name}
+          onChange={onChange}
+          value={value}
+          placeholder={placeholder}
+          onKeyDown={onKeyDown}
+        />
+        <Image
+          src={searchIcon}
+          alt="searchIcon"
+          className="absolute ml-11"
+        ></Image>
+        <AiFillCloseCircle
+          className="absolute right-11 text-dubgray"
+          size={20}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex relative items-center">
+        <input
+          className="flex justify-center text-16 rounded-5 font-normal placeholder-dubgray text-dubblack outline-none h-43 w-358 border border-[#E9ECEF] pl-35 py-12"
+          type={type}
+          name={name}
+          onChange={onChange}
+          value={value}
+          placeholder={placeholder}
+          onKeyDown={onKeyDown}
+        />
+        <Image
+          src={searchIcon}
+          alt="searchIcon"
+          className="absolute ml-11"
+        ></Image>
+      </div>
+    );
+  }
 }
