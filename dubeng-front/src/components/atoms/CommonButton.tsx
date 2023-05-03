96e7a7923328b81e9f628eca7bf9d66edaa2f900
eondo <vov06144@naver.com>
2023-05-03 10:19:43 +0900

    feat: fix storybook error and change DubButton, CommonButton storybook

--- dubeng-front/src/components/atoms/CommonButton.tsx
+++ dubeng-front/src/components/atoms/CommonButton.tsx
@@ -10,15 +10,15 @@ export default function CommonButton({ children, isDisabled }: Iprops) {
   if (isDisabled) {
     return (
       <button
-        disabled
-        className="rounded-8 bg-dubblue text-white text-16 px-48 py-8"
+        disabled={true}
+        className="rounded-8 border-1 border-dubblue bg-dubblue text-white text-16 px-48 py-8"
       >
         {children}
       </button>
     );
   }
   return (
-    <button className="rounded-8 bg-dubblue text-white text-16 px-48 py-8">
+    <button className="rounded-8 border-1 border-dubgraydeep bg-white text-dubgraydeep text-16 px-48 py-8">
       {children}
     </button>
   );
--- dubeng-front/src/components/atoms/DubButton.tsx
+++ dubeng-front/src/components/atoms/DubButton.tsx
@@ -16,9 +16,9 @@ export default function DubButton({ page }: Iprops) {
     );
   } else {
     return (
-      <button className="flex justify-center rounded-20 p-6 w-96 h-29 p-3 border-dubgray border-1 font-semibold text-dubgray">
+      <button className="flex text-14 rounded-20 p-6 pl-16 pr-10 border-1 border-dubgray font-semibold text-dubgray">
         <div className="flex justify-center items-center">
-          <p className="text-14">더빙하기</p>
+          <p>더빙하기</p>
           <MdArrowForwardIos size={16} color="dubgray" />
         </div>
       </button>
