2a50541495a212f78cc3a11360b89f9fdce34c3f
김동률 <lancelot1672@naver.com>
2023-05-03 21:07:00 +0900

    [BE] modify: swagger 2.9 -> 3.0

--- back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/SwaggerConfig.java
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/SwaggerConfig.java
@@ -12,15 +12,15 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import com.fasterxml.classmate.TypeResolver;
 
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     private final TypeResolver typeResolver;
@@ -31,27 +31,28 @@ public class SwaggerConfig {
 
     private ApiInfo commonInfo() {
         return new ApiInfoBuilder()
-                .title("목록 API")
-                //.description("")
-                //.license("leeys")
-                //.licenseUrl("http://leeys.tistory.com")
+                .title("Dubeng List REST API Server")
+                .description("더빙 목록API")
                 .version("1.0")
                 .build();
     }
 
     @Bean
     public Docket allApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Server serverLocal = new Server("local", "http://localhost:8080", "for local useages", Collections.emptyList(), Collections.emptyList());
+        Server serverDev = new Server("dev", "https:k8b208.p.ssafy.io", "for Production useages", Collections.emptyList(), Collections.emptyList());
+
+        return new Docket(DocumentationType.OAS_30)
                 .alternateTypeRules(AlternateTypeRules
                         .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .groupName("list")
-                .useDefaultResponseMessages(false)
+                .useDefaultResponseMessages(true)
+                .servers(serverLocal, serverDev)
+                .apiInfo(commonInfo())
                 .select()
-                //.apis(RequestHandlerSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("com.ssafy.dubengdublist.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ssafy.dubengdublist"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(commonInfo());
+                .build();
     }
 
     @Getter
---
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/Workaround.java
@@ -0,0 +1,35 @@
+package com.ssafy.dubengdublist.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openAPI = context.getSpecification();
+        Server localServer = new Server();
+        localServer.setDescription("Local");
+        localServer.setUrl("http://localhost:8080");
+
+        Server devServer = new Server();
+        devServer.setDescription("Production");
+        devServer.setUrl("https://k8b208.p.ssafy.io");
+        openAPI.setServers(Arrays.asList(localServer, devServer));
+
+        return openAPI;
+    }
+
+    @Override
+    public boolean supports(DocumentationType delimiter) {
+        return delimiter.equals(DocumentationType.OAS_30);
+    }
+}
