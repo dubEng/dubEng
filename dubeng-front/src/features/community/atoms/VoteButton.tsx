dd0649b795651588cb242d4b59a54ed585cae8be
김도언 <vov06144@naver.com>
2023-05-04 07:22:47 +0900

    feat: add community atoms

--- dubeng-front/src/features/community/atoms/VoteButton.tsx
+++ dubeng-front/src/features/community/atoms/VoteButton.tsx
@@ -1,3 +1,29 @@
-export default function VoteButton() {
-  return <></>;
+import { MdCheckCircle } from "react-icons/md";
+
+interface Iprops {
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+export default function VoteButton({ isSelected, onClick }: Iprops) {
+  if (isSelected) {
+    return (
+      <button
+        className="flex justify-center items-center w-140 h-30 px-16 py-8 rounded-8 text-16 text-white bg-dubcoral border-1 border-dubcoral"
+        onClick={onClick}
+      >
+        <p className="pb-1">투표 완료</p>
+        <MdCheckCircle className="ml-3" size={16} />
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className="flex justify-center items-center w-140 h-30 px-16 py-8 rounded-8 text-16 text-dubcoral bg-white border-1 border-dubcoral"
+        onClick={onClick}
+      >
+        <p className="pb-1">투표</p>
+      </button>
+    );
+  }
 }
