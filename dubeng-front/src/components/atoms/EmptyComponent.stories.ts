c54406618bde9e2c502b63d756c5d6ec92e68042
eondo <vov06144@naver.com>
2023-05-03 11:11:59 +0900

    feat: change css for EmptyComponent and ErrorCompoennt

--- dubeng-front/src/components/atoms/EmptyComponent.stories.ts
+++ dubeng-front/src/components/atoms/EmptyComponent.stories.ts
@@ -1,23 +1,39 @@
-// import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-// import EmptyComponent from './EmptyComponent';
+import EmptyComponent from "./EmptyComponent";
+import { EmptyType } from "../../enum/statusType";
 
-// const meta: Meta<typeof EmptyComponent> = {
-//     title: 'components/atoms/EmptyComponent',
-//     component: EmptyComponent,
-//   };
+const meta: Meta<typeof EmptyComponent> = {
+  title: "components/atoms/EmptyComponent",
+  component: EmptyComponent,
+};
 
-// export default meta;
+export default meta;
 
-// type Story = StoryObj<typeof EmptyComponent>;
+type Story = StoryObj<typeof EmptyComponent>;
 
-// export const Default: Story = {
-//   args: {
-//     value: "",
-//   },
-// };
-// export const TextInput: Story = {
-//   args: {
-//     value: "텍스트를 입력한 상태입니다.",
-//   },
-// };
+export const EMPTY_DUB_PRODUCT: Story = {
+  args: {
+    status: EmptyType.EMPTY_DUB_PRODUCT,
+  },
+};
+export const EMPTY_LIKE_DUB_PRODUCT: Story = {
+  args: {
+    status: EmptyType.EMPTY_LIKE_DUB_PRODUCT,
+  },
+};
+export const EMPTY_SCRAP_DUB_VIDEO: Story = {
+  args: {
+    status: EmptyType.EMPTY_SCRAP_DUB_VIDEO,
+  },
+};
+export const EMPTY_COMMENT: Story = {
+  args: {
+    status: EmptyType.EMPTY_COMMENT,
+  },
+};
+export const EMPTY_VOTE: Story = {
+  args: {
+    status: EmptyType.EMPTY_VOTE,
+  },
+};
--- dubeng-front/src/components/atoms/ErrorComponent.tsx
+++ dubeng-front/src/components/atoms/ErrorComponent.tsx
@@ -4,7 +4,7 @@ export default function ErrorComponent() {
       <p className="text-dubgray text-14">
         서버에서 요청을 처리하는데 실패하였습니다.
       </p>
-      <button className="mt-16 px-48 py-8 rounded-8 bg-dubgraylight border-dubblue text-dubblue text-16">
+      <button className="mt-16 px-48 py-8 rounded-8 bg-dubgraylight border-1 border-dubblue text-dubblue text-16">
         다시 시도
       </button>
     </div>
