5a5a933a1d131cb3bfa9278bb63d4c436c2bf0b1
최정온 <chjo0330@gmail.com>
2023-05-06 10:02:40 +0900

    feat: edit DubBox

--- dubeng-front/src/features/dubbing/organism/DubBox.tsx
+++ dubeng-front/src/features/dubbing/organism/DubBox.tsx
@@ -17,6 +17,10 @@ export default function DubBox() {
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [soundStatus, setSoundStatus] = useState<SoundType>(SoundType.DEFAULT);
 
+  const [recording, setRecording] = useState<boolean>(false);
+  const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+
+  // 브라우저 호환성 체크
   const [speechRecognitionSupported, setSpeechRecognitionSupported] = useState<
     boolean | null
   >(null);
@@ -55,20 +59,71 @@ export default function DubBox() {
     }
   }
 
-  function handleStartButton() {
-    SpeechRecognition.startListening();
+  function handleStartButton(recordingTime: number) {
+    // 스크립트 리셋
+    resetTranscript();
+
+    // STT 시작
+    SpeechRecognition.startListening({ continuous: true, language: "en-US" });
+
+    // 녹음 시작
+    setRecording(true);
+    const chunks: Blob[] = [];
+    const mediaStreamConstraints = { audio: true };
+
+    navigator.mediaDevices
+      .getUserMedia(mediaStreamConstraints)
+      .then((mediaStream) => {
+        const mediaRecorder = new MediaRecorder(mediaStream);
+        mediaRecorder.start();
+
+        mediaRecorder.addEventListener("dataavailable", (event) => {
+          chunks.push(event.data);
+        });
+
+        mediaRecorder.addEventListener("stop", () => {
+          setRecording(false);
+          const audioBlob = new Blob(chunks, { type: "audio/wav" });
+          setAudioBlob(audioBlob);
+        });
+
+        setTimeout(() => {
+          mediaRecorder.stop();
+        }, recordingTime);
+      });
   }
 
   function handleStopButton() {
     SpeechRecognition.stopListening();
   }
 
+  const sendRecording = async (blob: Blob) => {
+    const formData = new FormData();
+    formData.append('recording', blob, 'recording.wav');
+    
+    const response = await fetch('/api/recordings', {
+      method: 'POST',
+      body: formData,
+    });
+    
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const playRecording = () => {
+    if (audioBlob) {
+      const url = URL.createObjectURL(audioBlob);
+      const audio = new Audio(url);
+      audio.play();
+    }
+  };
+
   return (
     <div className="w-359 h-420 relative">
       <div>
         <div>Microphone: {listening ? "듣는 중" : "안 듣는 중"}</div>
         <div>
-          <button onClick={handleStartButton}>Start</button>
+          <button onClick={() => handleStartButton(3000)} disabled={recording}>Start</button>
         </div>
         <div>
           <button onClick={handleStopButton}>Stop</button>
@@ -76,7 +131,12 @@ export default function DubBox() {
         <div>
           <button onClick={resetTranscript}>Reset</button>
         </div>
-        <div>{transcript}</div>
+        <div>
+          <button onClick={playRecording} disabled={!audioBlob}>Play Recording</button>
+        </div>
+        <br />
+        <div>실시간 STT: {transcript}</div>
+        <div>녹음 상태: {recording ? "Recording..." : "not Recording"}</div>
       </div>
       <div className="flex justify-evenly">
         <PlayButton isPlaying={isPlaying} onClick={handlePlayButton} />
