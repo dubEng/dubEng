287f2cdfad6c9c855fe098866ea4246a71514882
eondo <vov06144@naver.com>
2023-05-09 00:16:40 +0900

    feat: add store states for tab index

--- dubeng-front/src/stores/store.ts
+++ dubeng-front/src/stores/store.ts
@@ -1,10 +1,14 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "../stores/user/userSlice";
 import scriptsPostSlice from "./manager/scriptsPostSlice";
+import communityTabSlice from "./community/communityTabSlice";
+import languageTabSlice from "./community/languageTabSlice";
 
 const reducers = combineReducers({
   user: userReducer,
   scriptsPostInfo: scriptsPostSlice,
+  communityTab: communityTabSlice,
+  languageTab: languageTabSlice,
 });
 
 export const store = configureStore({
