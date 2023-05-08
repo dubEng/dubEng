8fb9aa43034a717ce028a7cfdd5e23151f667eef
eondo <vov06144@naver.com>
2023-05-09 00:13:52 +0900

    feat: add next.js image tag settings

--- dubeng-front/next.config.js
+++ dubeng-front/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/front',
-}
+  images: {
+    domains: ["i.ytimg.com"],
+  },
+  basePath: "/front",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
