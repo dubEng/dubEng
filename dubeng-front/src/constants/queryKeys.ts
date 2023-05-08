67f1a50321c6e64fc38b8f76bcc783e7f1d60d2d
최정온 <chjo0330@gmail.com>
2023-05-09 00:22:57 +0900

    feat: add useDubRecordVideoInfoQuery

--- dubeng-front/src/constants/queryKeys.ts
+++ dubeng-front/src/constants/queryKeys.ts
@@ -6,3 +6,6 @@ export const CATEGORY = "/category";
 //signup
 export const NICKNAME = "/check";
 export const INTEREST_LIST = "/interest";
+
+//dubbing
+export const DUB_RECORD_VIDEO_INFO = "/dub/record/videoInfo"
