5359bbb42d11c2d4877b7979a1c5657dfbbbd8f5
lancelot1672 <lancelot1672@naver.com>
2023-05-08 15:34:42 +0900

    [FE] add: get LoginInfo

--- dubeng-front/src/apis/signup/mutations/useSignupPost.ts
+++ dubeng-front/src/apis/signup/mutations/useSignupPost.ts
@@ -8,8 +8,7 @@ const config = {
 
 const fetcher = (signupInfo: SignupInfo) =>
   axios
-    // .post("https://k8b208.p.ssafy.io/user" + `/auth/join`, signupInfo, config)
-    .post("http://localhost:9000/user" + `/auth/join`, signupInfo, config)
+    .post(process.env.NEXT_PUBLIC_BASE_URL + `/user/auth/join`, signupInfo, config)
     .then(({ data }) => data);
 
 const useSignupPost = () => {
