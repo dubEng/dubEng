a30a381c8e77faedc69ab1ee74d3e76822c95810
김동률 <lancelot1672@naver.com>
2023-05-08 23:32:24 +0900

    [BE] modify:swagger 3.0 -> 2.0

--- back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/SwaggerConfig.java
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.dubengdublist.config;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,42 +13,36 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import com.fasterxml.classmate.TypeResolver;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
 import java.util.List;
 
+@EnableSwagger2
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
 
     private final TypeResolver typeResolver;
-
-    public SwaggerConfig(TypeResolver typeResolver) {
-        this.typeResolver = typeResolver;
-    }
 
     private ApiInfo commonInfo() {
         return new ApiInfoBuilder()
                 .title("Dubeng List REST API Server")
                 .description("더빙 목록API")
-                .version("1.0")
+                .version("2.0")
                 .build();
     }
 
     @Bean
     public Docket allApi() {
-        Server serverLocal = new Server("local", "http://localhost:8080", "for local useages", Collections.emptyList(), Collections.emptyList());
-        Server serverDev = new Server("dev", "https:k8b208.p.ssafy.io", "for Production useages", Collections.emptyList(), Collections.emptyList());
 
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .alternateTypeRules(AlternateTypeRules
                         .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .groupName("list")
                 .useDefaultResponseMessages(true)
-                .servers(serverLocal, serverDev)
                 .apiInfo(commonInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ssafy.dubengdublist"))
