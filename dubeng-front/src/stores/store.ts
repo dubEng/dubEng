e76d0032d4709bc621e73a522fe53c2c7dbcbb54
최정온 <chjo0330@gmail.com>
2023-05-08 14:30:39 +0900

    feat: update store

--- dubeng-front/src/stores/store.ts
+++ dubeng-front/src/stores/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "../stores/user/userSlice";
 import scriptsPostSlice from "./manager/scriptsPostSlice";
+import dubbingReducer from "../stores/dubbing/dubbingSlice";
 
 const reducers = combineReducers({
   user: userReducer,
   scriptsPostInfo: scriptsPostSlice,
+  dubbing: dubbingReducer,
 });
 
 export const store = configureStore({
