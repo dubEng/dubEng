f8e85e14137fce631e120fbe2581f07de8fe4bc6
eondo <vov06144@naver.com>
2023-05-09 00:15:40 +0900

    feat: change DubSituation css style

--- dubeng-front/src/features/community/molecules/DubSituation.tsx
+++ dubeng-front/src/features/community/molecules/DubSituation.tsx
@@ -6,8 +6,8 @@ export default function DubSituation() {
     <div className="w-358 grid gap-8 grid-cols-2">
       <DubSituationCard type={SituationType.PLACE_1} />
       <DubSituationCard type={SituationType.PLACE_2} />
-      <DubSituationCard type={SituationType.EMOTION} />
       <DubSituationCard type={SituationType.COUNTRY} />
+      <DubSituationCard type={SituationType.EMOTION} />
     </div>
   );
 }
