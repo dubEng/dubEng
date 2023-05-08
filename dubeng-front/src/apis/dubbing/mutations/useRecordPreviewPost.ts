80643da0edd764a9373168e743486a2ebd3deddf
최정온 <chjo0330@gmail.com>
2023-05-09 04:37:30 +0900

    feat: update useRecordPreviewPost

--- dubeng-front/src/apis/dubbing/mutations/useRecordPreviewPost.ts
+++ dubeng-front/src/apis/dubbing/mutations/useRecordPreviewPost.ts
@@ -7,7 +7,7 @@ const config = {
 
 const fetcher = (formData: FormData) =>
   axios
-    .post("https://k8b208.p.ssafy.io" + `/record/save`, formData, config)
+    .post("https://k8b208.p.ssafy.io" + `/record/preview`, formData, config)
     .then(({ data }) => data);
 
 const useRecordPreviewPost = () => {
