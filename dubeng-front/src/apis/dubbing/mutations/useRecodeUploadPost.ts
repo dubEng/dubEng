a392ea89ae83d4c062b641bd5bf6cb4b83369025
최정온 <chjo0330@gmail.com>
2023-05-10 03:33:12 +0900

    feat: edit api request for env file

--- dubeng-front/src/apis/dubbing/mutations/useRecodeUploadPost.ts
+++ dubeng-front/src/apis/dubbing/mutations/useRecodeUploadPost.ts
@@ -7,7 +7,7 @@ const config = {
 
 const fetcher = (formData: FormData) =>
   axios
-    .post("https://k8b208.p.ssafy.io" + `/recode/upload`, formData, config)
+    .post(process.env.NEXT_PUBLIC_BASE_URL + `/recode/upload`, formData, config)
     .then(({ data }) => data);
 
 const useRecodeUploadPost = () => {
--- dubeng-front/src/apis/dubbing/queries/useDubRecordVideoInfoQuery.ts
+++ dubeng-front/src/apis/dubbing/queries/useDubRecordVideoInfoQuery.ts
@@ -4,7 +4,7 @@ import * as queryKeys from "@/constants/queryKeys";
 
 const fetcher = (videoId: number) =>
   axios
-    .get("https://k8b208.p.ssafy.io" + `/dub/record/${videoId}`)
+    .get(process.env.NEXT_PUBLIC_BASE_URL + `/dub/record/${videoId}`)
     .then(({data}) => data);
 
 const useDubRecordVideoInfoQuery = (videoId: number) => {
