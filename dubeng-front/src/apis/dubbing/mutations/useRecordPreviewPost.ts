0ef5815b9819a7dfbb58704e22f2381b58867959
최정온 <chjo0330@gmail.com>
2023-05-09 20:40:30 +0900

    feat: update useRecordPreviewPost

--- dubeng-front/src/apis/dubbing/mutations/useRecordPreviewPost.ts
+++ dubeng-front/src/apis/dubbing/mutations/useRecordPreviewPost.ts
@@ -7,16 +7,16 @@ const config = {
 
 const fetcher = (formData: FormData) =>
   axios
-    .post("https://k8b208.p.ssafy.io" + `/record/preview`, formData, config)
+    .post("https://k8b208.p.ssafy.io" + `/recode/upload`, formData, config)
     .then(({ data }) => data);
 
 const useRecordPreviewPost = () => {
   return useMutation(fetcher, {
     onSuccess: () => {
-      window.alert("비디오 저장 성공.");
+      console.log('녹음 파일 저장 성공');
     },
     onError: (error) => {
-      window.alert("문제 발생");
+      console.error('녹음 파일 저장에 실패하였습니다.');
     },
   });
 };
