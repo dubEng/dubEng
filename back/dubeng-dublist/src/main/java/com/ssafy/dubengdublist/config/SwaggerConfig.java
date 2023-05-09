0449db14b98a41f4dfccf579433598c1f46a999a
김아영 (KIM A YOUNG) <joen00@naver.com>
2023-05-09 14:00:30 +0900

    feat: delete annotation

--- back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/SwaggerConfig.java
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/config/SwaggerConfig.java
@@ -59,8 +59,6 @@ public class SwaggerConfig {
 
         @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
         private String size;
-//
-//        @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
-//        private List<String> sort;
+
     }
 }
