13b58843559bbb367e36de8d992928c13c6cc54b
eondo <vov06144@naver.com>
2023-05-10 10:01:36 +0900

    refactor: change api url by adding env file

---
+++ dubeng-front/src/apis/community/queries/useRecommendDubVideoListQuery.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import * as queryKeys from "@/constants/queryKeys";
+
+const fetcher = (langType: string) =>
+  axios
+    .get(process.env.NEXT_PUBLIC_BASE_URL + `/recommand/${langType}`, {
+      params: {
+        page: 1,
+        size: 10,
+      },
+    })
+    .then((res) => {
+      return res;
+    });
+
+const useRecommendDubVideoListQuery = (langType: string) => {
+  return useQuery(queryKeys.RECOMMEND_DUV_VIDEO, () => fetcher(langType));
+};
+
+export default useRecommendDubVideoListQuery;
