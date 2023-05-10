3e85c08464168495d4710326c3d32a2c9264186f
최정온 <chjo0330@gmail.com>
2023-05-10 13:44:06 +0900

    feat: add 404 page

---
+++ dubeng-front/src/pages/404.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="container mx-auto">
+        <p className="text-dubblack text-14 text-center">
+          페이지를 찾을 수 없습니다.
+        </p>
+    </div>
+  );
+}
