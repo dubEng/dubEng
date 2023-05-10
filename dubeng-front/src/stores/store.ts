2a90a469be53bb6a10757b23993855a79e58143e
최정온 <chjo0330@gmail.com>
2023-05-10 09:46:29 +0900

    feat: redux-persist 설정

--- dubeng-front/src/stores/store.ts
+++ dubeng-front/src/stores/store.ts
@@ -1,4 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 import userReducer from "../stores/user/userSlice";
 import scriptsPostSlice from "./manager/scriptsPostSlice";
 import communityTabSlice from "./community/communityTabSlice";
@@ -10,11 +12,21 @@ const reducers = combineReducers({
   scriptsPostInfo: scriptsPostSlice,
   communityTab: communityTabSlice,
   languageTab: languageTabSlice,
-  signupInfo : signupSlice,
+  signupInfo: signupSlice,
 });
 
+const persistConfig = {
+  timeout: 100,
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
