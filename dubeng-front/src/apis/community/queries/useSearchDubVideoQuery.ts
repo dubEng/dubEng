02ffa3212b72b7fda04524e67b8ddf29e58c74bb
eondo <vov06144@naver.com>
2023-05-10 14:25:54 +0900

    feat: add search list api

---
+++ dubeng-front/src/apis/community/queries/useSearchDubVideoQuery.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import * as queryKeys from "@/constants/queryKeys";
+
+const fetcher = (
+  contentsSearch: number[],
+  langType: string,
+  pageParam: string,
+  size: string,
+  title: string
+) =>
+  axios
+    .get(process.env.NEXT_PUBLIC_BASE_URL + `/contents/search/${langType}`, {
+      params: {
+        contentsSearch: contentsSearch,
+        page: pageParam,
+        size: size,
+        title: title,
+      },
+    })
+    .then((res) => {
+      return res;
+    });
+
+const useSearchDubVideoQuery = (
+  contentsSearch: number[],
+  langType: string,
+  size: string,
+  title: string
+) => {
+  return useQuery(
+    [queryKeys.SEARCH_DUB_VIDEO, contentsSearch, langType, size, title],
+    ({ pageParam = 0 }) =>
+      fetcher(contentsSearch, langType, title, size, pageParam)
+  );
+};
+
+export default useSearchDubVideoQuery;
