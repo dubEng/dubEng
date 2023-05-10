37c889e005b0472cf9bfa16ce7bc2d8d1253fbd9
최정온 <chjo0330@gmail.com>
2023-05-10 10:05:10 +0900

    feat: temp mypage

--- dubeng-front/src/pages/mypage/index.tsx
+++ dubeng-front/src/pages/mypage/index.tsx
@@ -1,3 +1,12 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../stores/store";
+
 export default function MyPage(){
-  return (<>마이페이지</>);
+  const userId = useSelector((state: RootState) => state.user.userId);
+  const nickname = useSelector((state: RootState) => state.user.nickname);
+
+  return (<>
+  userId: {userId} <br/>
+  nickname: {nickname}
+  </>);
 }
