3300b15634c2091163c53588376c448a75a73bf9
최정온 <chjo0330@gmail.com>
2023-05-11 11:11:52 +0900

    hotfix: dubbing test

--- dubeng-front/src/components/organism/DubVideoSlider.tsx
+++ dubeng-front/src/components/organism/DubVideoSlider.tsx
@@ -9,38 +9,44 @@ interface Iprops {
 export default function DubVideoSlider({ isOpen, setOpen }: Iprops) {
   const dumpList = [
     {
-      id: 1,
+      id: 160,
       title: "커리지 | 인사해요",
       thumbnail: "",
       runtime: "2분 3초",
     },
     {
-      id: 2,
+      id: 161,
       title: "언도의 모험",
       thumbnail: "",
       runtime: "2분 2초",
     },
     {
-      id: 3,
+      id: 162,
       title: "동동이의 세계일주",
       thumbnail: "",
       runtime: "2분 1초",
     },
     {
-      id: 4,
+      id: 163,
       title: "아영이의 국내일주",
       thumbnail: "",
       runtime: "2분 15초",
     },
     {
-      id: 5,
-      title: "지희의 CA 체험기",
+      id: 171,
+      title: "오! 완전 무대를 뒤집어 놓으셨다.",
       thumbnail: "",
       runtime: "2분 2초",
     },
     {
-      id: 6,
-      title: "자민이의 안산 정벌",
+      id: 175,
+      title: "[인턴] - 안하는 것보다 늦게 하는 것이 낫다",
+      thumbnail: "",
+      runtime: "2분 3초",
+    },
+    {
+      id: 176,
+      title: "[어바웃 타임] - 부모님 오시기 전",
       thumbnail: "",
       runtime: "2분 3초",
     },
@@ -52,7 +58,7 @@ export default function DubVideoSlider({ isOpen, setOpen }: Iprops) {
         <Sheet.Container>
           <Sheet.Header />
           <Sheet.Content>
-            <div className="mx-16">
+            <div className="mx-16 h-full">
               <p className="flex justify-start text-19 font-bold mb-16">
                 이 영상, 지금 더빙 어때요?
               </p>
