d27d15eab77c26bc799d626d6a780ca811ea0f1d
eondo <vov06144@naver.com>
2023-05-11 17:05:36 +0900

    feat: add search query

--- dubeng-front/src/apis/community/queries/useSearchDubVideoQuery.ts
+++ dubeng-front/src/apis/community/queries/useSearchDubVideoQuery.ts
@@ -2,25 +2,33 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 
+import qs from "qs";
+
 const fetcher = (
   contentsSearch: number[],
   langType: string,
   pageParam: string,
   size: string,
   title: string
-) =>
-  axios
-    .get(process.env.NEXT_PUBLIC_BASE_URL + `/contents/search/${langType}`, {
-      params: {
-        contentsSearch: contentsSearch,
-        page: pageParam,
-        size: size,
-        title: title,
-      },
-    })
+) => {
+  const queryArray = qs.stringify(contentsSearch, { arrayFormat: "repeat" });
+  return axios
+    .get(
+      process.env.NEXT_PUBLIC_BASE_URL + `/dub/contents/search/${langType}`,
+      {
+        params: {
+          queryArray,
+          page: pageParam,
+          size: size,
+          title: title,
+        },
+      }
+    )
     .then(({ data }) => {
+      console.log("{ search쿼리data }확인", data);
       return data;
     });
+};
 
 const useSearchDubVideoQuery = (
   contentsSearch: number[],
@@ -30,7 +38,7 @@ const useSearchDubVideoQuery = (
 ) => {
   return useQuery(
     [queryKeys.SEARCH_DUB_VIDEO, contentsSearch, langType, size, title],
-    ({ pageParam = 0 }) =>
+    ({ pageParam = "0" }) =>
       fetcher(contentsSearch, langType, pageParam, size, title)
   );
 };
