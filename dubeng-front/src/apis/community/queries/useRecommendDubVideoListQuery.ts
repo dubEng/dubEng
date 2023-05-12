afaa4f2aa34739c5f64881380dd1d8b2e35e0828
eondo <vov06144@naver.com>
2023-05-12 17:19:25 +0900

    feat: revise GET recommend video list query

--- dubeng-front/src/apis/community/queries/useRecommendDubVideoListQuery.ts
+++ dubeng-front/src/apis/community/queries/useRecommendDubVideoListQuery.ts
@@ -1,18 +1,24 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores/store";
 
-const fetcher = (langType: string) =>
-  axios
-    .get(process.env.NEXT_PUBLIC_BASE_URL + `/dub/contents/recommand/${langType}`, {
+const fetcher = (langType: string) => {
+  const userId = useSelector((state: RootState) => {
+    return state.user.userId;
+  });
+
+  return axios
+    .get(process.env.NEXT_PUBLIC_BASE_URL + `recommend/contents/${langType}`, {
       params: {
-        page: 0,
-        size: 10,
+        userId: userId,
       },
     })
     .then((res) => {
       return res;
     });
+};
 
 const useRecommendDubVideoListQuery = (langType: string) => {
   return useQuery([queryKeys.RECOMMEND_DUB_VIDEO_LIST, langType], () =>
