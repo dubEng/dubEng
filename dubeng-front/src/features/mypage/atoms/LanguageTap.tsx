0c608fa3336fb5954c258cf3adca9dafa9e173fc
eondo <vov06144@naver.com>
2023-05-14 19:05:31 +0900

    feat: add LanguageTap component

--- dubeng-front/src/features/mypage/atoms/LanguageTap.tsx
+++ dubeng-front/src/features/mypage/atoms/LanguageTap.tsx
@@ -1,3 +1,60 @@
-export default function LanguageTap(){
-  return (<></>);
+import { LangType } from "@/enum/statusType";
+import { useState } from "react";
+
+interface Iprops {
+  myPageLangIndex: string;
+  handleMyPageLangIndex: (presentIndex: LangType) => void;
+}
+
+export default function LanguageTap({
+  myPageLangIndex,
+  handleMyPageLangIndex,
+}: Iprops) {
+  function handleEnglishTab() {
+    console.log("English 누름");
+    handleMyPageLangIndex(LangType.ENGLISH);
+  }
+  function handleKoreanTab() {
+    console.log("한국어 누름");
+    handleMyPageLangIndex(LangType.KOREAN);
+  }
+
+  if (myPageLangIndex === LangType.ENGLISH) {
+    return (
+      <div>
+        <div className="flex ml-4 justify-between">
+          <div className="flex">
+            <p className="text-16 text-dubblack font-semibold">English</p>
+            <p
+              className="ml-16 text-16 text-dubblack font-semibold"
+              onClick={handleKoreanTab}
+            >
+              한국어
+            </p>
+          </div>
+        </div>
+        <div className="mt-8 z-0 w-358 h-2 bg-dubgraylight"></div>
+        <div className="relative -top-2 w-65 h-2 bg-dubblack"></div>
+      </div>
+    );
+  } else if (myPageLangIndex === LangType.KOREAN) {
+    return (
+      <div>
+        <div className="flex ml-4 justify-between">
+          <div className="flex">
+            <p
+              className="text-16 text-dubblack font-semibold"
+              onClick={handleEnglishTab}
+            >
+              English
+            </p>
+            <p className="ml-16 text-16 text-dubblack font-semibold">한국어</p>
+          </div>
+        </div>
+        <div className="mt-8 z-0 w-358 h-2 bg-dubgraylight"></div>
+        <div className="ml-69 relative -top-2 w-52 h-2 bg-dubblack"></div>
+      </div>
+    );
+  }
+  return <></>;
 }
--- dubeng-front/src/pages/mypage/index.tsx
+++ dubeng-front/src/pages/mypage/index.tsx
@@ -2,14 +2,27 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../stores/store";
 import DubProductCard from "@/features/mypage/molecules/DubProductCard";
 import WeBareBears from "../../../public/images/dump/webarebears_image.png";
+import { SetStateAction, useState } from "react";
+import { LangType } from "@/enum/statusType";
+import LanguageTap from "@/features/mypage/atoms/LanguageTap";
 
 export default function MyPage() {
   const userId = useSelector((state: RootState) => state.user.userId);
   const nickname = useSelector((state: RootState) => state.user.nickname);
 
+  const [myPageLangIndex, setMyPageLangIndex] = useState(LangType.ENGLISH);
+
+  function handleMyPageLangIndex(presentIndex: LangType) {
+    console.log("handle 실행됨", myPageLangIndex);
+    setMyPageLangIndex(presentIndex);
+  }
   return (
     <div>
       <div className="w-200 h-500">하이</div>
+      <LanguageTap
+        myPageLangIndex={myPageLangIndex}
+        handleMyPageLangIndex={handleMyPageLangIndex}
+      />
       <DubProductCard
         title={"안녕안녕안녕하세요"}
         thumbnail={""}
