de3a67a7d02dba958bb526342dfd702f92e23e56
김동률 <lancelot1672@naver.com>
2023-05-14 20:27:59 +0900

    [FE] add: logout page

---
+++ dubeng-front/src/apis/login/queries/useLogoutQuery.ts
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { useMutation } from "react-query";
+import axios from "axios";
+import { userLogout } from "@/stores/user/userSlice";
+
+const fetcher = async () =>{
+  const {data} = await axios
+    .get(process.env.NEXT_PUBLIC_BASE_URL + `/user/auth/logout`);
+    
+    return data
+}
+
+const useUserInfoPost = () => {
+  const dispatch = useDispatch();
+
+  return useMutation(() => fetcher(), {
+    onSuccess: (data) => {
+      dispatch(userLogout);
+    },
+    onError: (error) => {
+      console.log("로그아웃 실패");
+    },
+  });
+};
+
+export default useUserInfoPost;
