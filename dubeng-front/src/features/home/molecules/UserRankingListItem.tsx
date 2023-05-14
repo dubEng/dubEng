1729c3226d6f6489231fe9f44ff0b42f56a58fbe
최정온 <chjo0330@gmail.com>
2023-05-14 15:48:28 +0900

    feat: update home-page

--- dubeng-front/src/features/home/molecules/UserRankingListItem.tsx
+++ dubeng-front/src/features/home/molecules/UserRankingListItem.tsx
@@ -1,5 +1,6 @@
 import { UserRanking } from "@/types/UserRanking";
 import Image from "next/image";
+import DefaultImage from "../../../../public/images/default/mic_profile.png";
 
 export default function UserRankingListItem({
   ranking,
@@ -11,16 +12,23 @@ export default function UserRankingListItem({
 }: UserRanking) {
   return (
     <div className="grid grid-cols-12 h-38 gap-8 mb-16">
-      <div className="text-14 text-dubblack col-span-2 flex items-center justify-center">
+      <div className="text-14 text-dubblack col-span-1 flex items-center justify-center">
         {ranking + 1}
       </div>
-      <div className="flex justify-end">
-        {/* <img src={imageUrl} width={38} height={38} className="rounded-md"></img> */}
-        {/* <Image src={imageUrl} alt={"profile"} width={38} height={38} className="rounded-md" /> */}
+      <div className="grid col-span-2 justify-items-end">
+        <Image
+          src={imageUrl ?? DefaultImage}
+          alt={"profile"}
+          width={38}
+          height={38}
+          className="rounded-md"
+        />
       </div>
       <div className="col-span-5 space-y-10">
-        <div className="text-14 text-dubblack">{nickname}</div>
-        <div className="text-10 text-dubgray">{introduce}</div>
+        <div className="flex flex-col ">
+          <p className="text-14 text-dubblack">{nickname}</p>
+          <p className="text-10 text-dubgray">{introduce}</p>
+        </div>
       </div>
       <div className="text-14 text-dubblack flex items-center justify-center col-span-2">
         {recordingTime}
