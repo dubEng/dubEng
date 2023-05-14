97080d93d17ef9e99aa074968ce0763dcf9b1fe0
eondo <vov06144@naver.com>
2023-05-14 22:39:09 +0900

    feat: add components

--- dubeng-front/src/pages/mypage/index.tsx
+++ dubeng-front/src/pages/mypage/index.tsx
@@ -5,34 +5,103 @@ import WeBareBears from "../../../public/images/dump/webarebears_image.png";
 import { SetStateAction, useState } from "react";
 import { LangType } from "@/enum/statusType";
 import LanguageTap from "@/features/mypage/atoms/LanguageTap";
+import MyPageProfile from "@/features/mypage/organism/MyPageProfile";
+import TagButton from "@/components/atoms/TagButton";
+import { MdArrowForwardIos } from "react-icons/md";
+import MyDubProductList from "@/features/mypage/organism/MyDubProductList";
 
 export default function MyPage() {
   const userId = useSelector((state: RootState) => state.user.userId);
   const nickname = useSelector((state: RootState) => state.user.nickname);
 
-  const [myPageLangIndex, setMyPageLangIndex] = useState(LangType.ENGLISH);
+  const interests = [
+    {
+      categoryName: "판타지",
+    },
+    {
+      categoryName: "애니메이션",
+    },
+    {
+      categoryName: "어하이",
+    },
+    {
+      categoryName: "학교에서",
+    },
+    {
+      categoryName: "회사에서",
+    },
+    {
+      categoryName: "오우",
+    },
+  ];
 
-  function handleMyPageLangIndex(presentIndex: LangType) {
-    console.log("handle 실행됨", myPageLangIndex);
-    setMyPageLangIndex(presentIndex);
-  }
   return (
-    <div>
-      <div className="w-200 h-500">하이</div>
-      <LanguageTap
-        myPageLangIndex={myPageLangIndex}
-        handleMyPageLangIndex={handleMyPageLangIndex}
+    <div className="static h-full px-16 bg-white mt-57 mb-61">
+      <MyPageProfile
+        nickname={"닉네임"}
+        description={"한 줄 소개입니다"}
+        profileImage={""}
+        totalRecTime={20}
+        recordCount={10}
       />
+
+      <div className="mt-24 h-10 bg-[#F5F5F5]"></div>
+
+      <p className="flex justify-start text-19 font-bold mt-24 mb-16">
+        선호 장르
+      </p>
+      <div className="flex flex-wrap">
+        {interests.map((tag: { categoryName: string }, idx: number) => (
+          <TagButton
+            onClick={() => {}}
+            id={0}
+            key={idx}
+            name={tag.categoryName}
+            isSelected={true}
+          />
+        ))}
+      </div>
+      <p className="flex justify-start text-19 font-bold mt-24 mb-16">
+        이달의 캘린더
+      </p>
+      <div>캘린더 들어가는 자리</div>
+      <div className="h-10 bg-[#F5F5F5]"></div>
+
+      <div className="flex items-center justify-between">
+        <p className="flex justify-start text-19 font-bold mt-24 mb-16">
+          나의 상영관
+        </p>
+        <button className="mt-24 mb-16 flex justify-center items-center">
+          <p className="text-dubblack text-14 ">모두보기</p>
+          <MdArrowForwardIos width={16} height={16} className="text-dubgray" />
+        </button>
+      </div>
+      <MyDubProductList />
+      <div className="flex items-center justify-between">
+        <p className="flex justify-start text-19 font-bold mt-24 mb-16">
+          좋아요한 더빙 목록
+        </p>
+        <button className="mt-24 mb-16 flex justify-center items-center">
+          <p className="text-dubblack text-14 ">모두보기</p>
+          <MdArrowForwardIos width={16} height={16} className="text-dubgray" />
+        </button>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <p className="flex justify-start text-19 font-bold mt-24 mb-16">
+          저장한 콘텐츠 목록
+        </p>
+        <button className="mt-24 mb-16 flex justify-center items-center">
+          <p className="text-dubblack text-14 ">모두보기</p>
+          <MdArrowForwardIos width={16} height={16} className="text-dubgray" />
+        </button>
+      </div>
       <DubProductCard
         title={"안녕안녕안녕"}
         thumbnail={""}
         playCount={11}
         updatedDate={""}
       />
-      <div className="h-screen bg-white mt-57 mb-61">
-        userId: {userId} <br />
-        nickname: {nickname}
-      </div>
     </div>
   );
 }
