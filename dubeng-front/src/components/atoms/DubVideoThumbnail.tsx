557ca88ee1df392e21a00f971c9cd0094848bef2
eondo <vov06144@naver.com>
2023-05-15 22:13:18 +0900

    fix: fix image size

--- dubeng-front/src/components/atoms/DubVideoThumbnail.tsx
+++ dubeng-front/src/components/atoms/DubVideoThumbnail.tsx
@@ -13,11 +13,12 @@ export default function DubVideoThumbnail({ id, title, thumbnail }: Iprops) {
     <div className="relative object-cover">
       <Image
         src={thumbnail}
-        className="rounded-18"
+        className="rounded-18 w-272 h-152"
         alt={title}
         width={272}
         height={152}
       />
+      <div className="opacity-50 absolute top-0 left-0 w-251 h-152 rounded-18"></div>
       <p className="absolute bottom-16 left-16 right-16 text-white font-semibold text-12 line-clamp-1">
         {title}
       </p>
