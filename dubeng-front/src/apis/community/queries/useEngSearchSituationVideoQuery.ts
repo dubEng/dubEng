8c259f9801485d98d7bb24d720649ca80fa68aac
eondo <vov06144@naver.com>
2023-05-15 22:02:45 +0900

    feat: add situation card api by language

---
+++ dubeng-front/src/apis/community/queries/useEngSearchSituationVideoQuery.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useQuery, useQueryClient } from "react-query";
+import * as queryKeys from "@/constants/queryKeys";
+
+import qs from "qs";
+
+const fetcher = async (
+  categoryId: null | number,
+  langType: string,
+  pageParam: number,
+  size: number,
+  title: null | string
+) => {
+  const queryArray = qs.stringify(
+    { contentsSearch: [categoryId] },
+    { arrayFormat: "repeat" }
+  );
+  const { data } = await axios.get(
+    process.env.NEXT_PUBLIC_BASE_URL +
+      `/dub/contents/search/${langType}` +
+      `?${queryArray}`,
+    {
+      params: {
+        page: pageParam,
+        size: size,
+        title: title,
+      },
+    }
+  );
+
+  console.log("data:", data);
+  return data;
+};
+
+const useEngSearchSituationVideoQuery = (
+  categoryId: null | number,
+  langType: string,
+  size: number,
+  title: string
+) => {
+  // const queryClient = useQueryClient();
+  console.log("useEngSearchSituation 안", categoryId);
+  return useQuery(
+    [queryKeys.SEARCH_SITUATION_VIDEO, categoryId],
+    ({ pageParam = 0 }) =>
+      fetcher(categoryId, langType, pageParam, size, title),
+    { enabled: !!categoryId }
+  );
+};
+
+export default useEngSearchSituationVideoQuery;
