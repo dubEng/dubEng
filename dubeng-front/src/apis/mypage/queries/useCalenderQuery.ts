394fed36ccbeb950aca2e4c416f5e7b0e7581a1c
김동률 <lancelot1672@naver.com>
2023-05-16 01:44:31 +0900

    [FE] add-calender-api

--- dubeng-front/src/apis/mypage/queries/useCalenderQuery.ts
+++ dubeng-front/src/apis/mypage/queries/useCalenderQuery.ts
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 import { useSelector } from "react-redux";
 import { RootState } from "@/stores/store";
+import { Attendance } from "@/features/mypage/atoms/MyCalendar";
 
 const fetcher = async(accessToken: string, month: number) => {
   axios.defaults.headers.common["Authorization"] = accessToken;
@@ -16,12 +17,12 @@ const fetcher = async(accessToken: string, month: number) => {
     return data;
 };
 
-const useCalenderQuery = (month : number) => {
-  const {accessToken } = useSelector((state: RootState) => state.user);
+const useCalenderQuery = (attendence : Attendance) => {
+  const { accessToken } = useSelector((state: RootState) => state.user);
 
   return useQuery(
-    [queryKeys.CALENDAR_ATTENDANCE_LIST, accessToken, month],
-    () => fetcher(accessToken, month)
+    [queryKeys.CALENDAR_ATTENDANCE_LIST, accessToken, attendence.month],
+    () => fetcher(accessToken, attendence.month)
   );
 };
 
