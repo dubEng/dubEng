7bab497d488fa78e51c8889a6405c7f13d2b93e4
최정온 <chjo0330@gmail.com>
2023-05-16 04:44:18 +0900

    feat: add myDubbingProductPage

--- dubeng-front/src/pages/mypage/my-dubbing-product.tsx
+++ dubeng-front/src/pages/mypage/my-dubbing-product.tsx
@@ -1,14 +1,75 @@
-import Header from "@/components/atoms/Header";
 import { LangType } from "@/enum/statusType";
 import LanguageTap from "@/features/mypage/atoms/LanguageTap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../stores/store";
+import useMyDubProductListMutation from "@/apis/mypage/mutations/useMyDubProductListMutationts";
+import { ScaleLoader } from "react-spinners";
+import ErrorComponent from "@/components/atoms/ErrorComponent";
+import Link from "next/link";
+import DubProductListItem from "@/components/molecules/DubProductListItem";
 
 export default function myDubbingProductPage() {
   const [myPageLangIndex, setMyPageLangIndex] = useState(LangType.ENGLISH);
 
+  const userId = useSelector((state: RootState) => state.user.userId);
+
+  const { mutateAsync, isLoading, isError } = useMyDubProductListMutation();
+
+  const [myProductList, setMyProductList] = useState<any>(null);
+
+  useEffect(() => {
+    if (userId) {
+      async function getMyDubProductList() {
+        const payload = {
+          userId: userId,
+          isPublic: false,
+          isLimit: false,
+          lanType: myPageLangIndex,
+        };
+
+        const { data } = await mutateAsync(payload);
+        setMyProductList(data);
+      }
+
+      getMyDubProductList();
+    }
+  }, [myPageLangIndex]);
+
+  useEffect(() => {
+    if (userId) {
+      async function getMyDubProductList() {
+        const payload = {
+          userId: userId,
+          isPublic: false,
+          isLimit: false,
+          lanType: myPageLangIndex,
+        };
+
+        const { data } = await mutateAsync(payload);
+        setMyProductList(data);
+      }
+
+      getMyDubProductList();
+    }
+  }, [userId]);
+
   function handleMyPageLangIndex(presentIndex: LangType) {
+    console.log('presentIndex', presentIndex);
     console.log("handle 실행됨", myPageLangIndex);
     setMyPageLangIndex(presentIndex);
+  }
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center my-16">
+        <ScaleLoader color="#FF6D60" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <ErrorComponent onClick={() => {}} retry={false} />;
   }
 
   return (
@@ -17,6 +78,22 @@ export default function myDubbingProductPage() {
         myPageLangIndex={myPageLangIndex}
         handleMyPageLangIndex={handleMyPageLangIndex}
       />
+      {myProductList &&
+        myProductList.map((item: any) => (
+          <Link href={`/community/shorts/product/${item.id}`}>
+            <DubProductListItem
+              title={item.title}
+              thumbnail={item.thumbnail}
+              playCount={item.playCount}
+              recordId={item.id}
+              key={item.id}
+              createdDate={item.updatedDate}
+              runtime={item.runtime}
+              profileImage={item.profileImage}
+              nickname={item.nickname}
+            />
+          </Link>
+        ))}
     </div>
   );
 }
