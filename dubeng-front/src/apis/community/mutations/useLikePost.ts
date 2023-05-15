f0bec27cb6e3c9093a05e9c6db1e4ddd22f22dbe
eondo <vov06144@naver.com>
2023-05-16 06:32:41 +0900

    feat: add like api

---
+++ dubeng-front/src/apis/community/mutations/useLikePost.ts
@@ -0,0 +1,37 @@
+import { useMutation } from "react-query";
+import axios from "axios";
+import { RootState } from "@/stores/store";
+import { useSelector } from "react-redux";
+
+interface requestParams {
+  recordId: number;
+  userId: string;
+}
+
+const fetcher = async (payload: requestParams) => {
+  console.log("useLikePost안에서 확인", payload.userId);
+  const { data } = await axios.post(
+    process.env.NEXT_PUBLIC_BASE_URL +
+      `/dub/community/like/${payload.recordId}`,
+    {
+      params: {
+        userId: payload.userId,
+      },
+    }
+  );
+  console.log("useLikePost", data);
+  return data;
+};
+
+const useLikePost = () => {
+  return useMutation(fetcher, {
+    onSuccess: () => {
+      window.alert("좋아요가 정상적으로 반영되었습니다.");
+    },
+    onError: (error) => {
+      window.alert("좋아요에 실패하였습니다.");
+    },
+  });
+};
+
+export default useLikePost;
