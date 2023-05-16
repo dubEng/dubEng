ec92f935c8ad735703daf140597099d2be3d6b28
lancelot1672 <lancelot1672@naver.com>
2023-05-17 04:27:34 +0900

    [FE] 띄움

--- dubeng-front/src/features/mission/organism/MissionKitchen.tsx
+++ dubeng-front/src/features/mission/organism/MissionKitchen.tsx
@@ -18,30 +18,29 @@ import KitchenTooltipBox from '../atoms/KitchenTooltipBox';
 extend({ EffectComposer, RenderPass, UnrealBloomPass , ShaderPass, SSAOPass, SSRPass });
 export default function MissionKitchen() {
      //미션 목록을 불러옵니다. API 요청이 들어갈 부분
-    const renderer = new WebGLRenderer({ antialias: true });
   
     const [rayTracing, setRayTracing] = useState(false);
     const [mywidth, setMyWidth] = useState(350);
     const [myheight,setMyHeight] = useState(300);
   
     return (
-        
-        <Canvas camera={{position: [1.1413,3.0704,1.95585] }} gl={renderer} id="Mycanvas">
-        <Suspense fallback={null}>
-            <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0.1,0.1,0.1]}>
-                <meshStandardMaterial color="#FFB679" />
-            </Plane>
-            <KitchenModel url="/assets/b2.glb" Rayss={rayTracing} myHeight={myheight} myWidth={mywidth}/>
-            <KitchenTooltipBox />
-        </Suspense>
-        <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
-        <OrbitControls  />
-        {/* <KitchenFilterEffect /> */}
-        <ambientLight intensity={0.3} color={'#FFA07A'} /> 
-        <pointLight position={[4, 1.75, 0.7]} intensity={1.7} color={'#FFAFA7'}/>
-        <pointLight position={[0.7, 1.1, 0.4]} intensity={0.8} color={'#FFFACD'} />
-        <pointLight position={[0.5, 1.7, -1]} intensity={1.5} color={'#FFAFA7'} />  
-        <ambientLight intensity={1.3} color={'red'} /> 
+        <Canvas camera={{position: [1.1413,3.0704,1.95585] }} style={{borderRadius : 10, border: '1px solid #F5F6F8'}}>
+            <Suspense fallback={null}>
+                <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0.1,0.1,0.1]}>
+                    <meshStandardMaterial color="#FFB679" />
+                </Plane>
+                <KitchenModel url="/assets/b2.glb" Rayss={rayTracing} myHeight={300} myWidth={350}/>
+                {/* <KitchenTooltipBox /> */}
+            </Suspense>
+            <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
+            <OrbitControls makeDefault dampingFactor={0.9} minDistance={5} maxDistance={20}
+                                    minAzimuthAngle={-Math.PI/4} maxAzimuthAngle={Math.PI/4} minPolarAngle={0} maxPolarAngle={Math.PI/3}  />
+            {/* <KitchenFilterEffect /> */}
+            <ambientLight intensity={0.3} color={'#FFA07A'} /> 
+            <pointLight position={[4, 1.75, 0.7]} intensity={1.7} color={'#FFAFA7'}/>
+            <pointLight position={[0.7, 1.1, 0.4]} intensity={0.8} color={'#FFFACD'} />
+            <pointLight position={[0.5, 1.7, -1]} intensity={1.5} color={'#FFAFA7'} />  
+            {/* <ambientLight intensity={1.3} color={'red'} />  */}
         </Canvas>
     )
 //   const canvas = useMemo(() => (
--- dubeng-front/src/pages/mission/index.tsx
+++ dubeng-front/src/pages/mission/index.tsx
@@ -1,14 +1,22 @@
-import MissionKitchen from "@/features/mission/organism/MissionKitchen";
 import MissionList from "@/features/mission/organism/MissionList";
+import React, { Suspense } from 'react';
+
+import { Canvas,ReactThreeFiber,Props, extend, useLoader, useThree,  } from '@react-three/fiber';
+import { OrbitControls, Stats,Html,ContactShadows,Plane, Gltf } from '@react-three/drei';
+
+import KitchenModel from "@/features/mission/atoms/KitchenModel";
+import KitchenTooltipBox from "@/features/mission/atoms/KitchenTooltipBox";
+import MissionKitchen from "@/features/mission/organism/MissionKitchen";
 
 export default function MissionPage() {
   return (
     <div className="container mx-auto h-screen bg-white mt-57 mb-61">
       <p className="flex justify-start mx-16 text-19 font-bold mt-24 mb-16">
         나의 섬
-        <MissionKitchen/>
       </p>
-
+      <div className="h-350 rounded-10">
+        <MissionKitchen/> 
+      </div>
       <p className="flex justify-start mx-16 text-19 font-bold mt-24 mb-16">
         도전과제
       </p>
@@ -16,4 +24,3 @@ export default function MissionPage() {
     </div>
   );
 }
-
