333eb0adf3261e7fc417b0ac1e3788724bf35fdb
최정온 <chjo0330@gmail.com>
2023-05-18 05:55:07 +0900

    feat: shorts update

--- dubeng-front/src/apis/community/queries/useCommunityShortsQuery.ts
+++ dubeng-front/src/apis/community/queries/useCommunityShortsQuery.ts
@@ -2,20 +2,22 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 
-const fetcher = (page: number, size: number) =>
+const fetcher = () =>
   axios
-    .get(process.env.NEXT_PUBLIC_BASE_URL + `/dub/community/shorts`,      {
+    .get(process.env.NEXT_PUBLIC_BASE_URL + `/dub/community/shorts`, {
       params: {
-        page: page,
-        size: size,
+        page: 0,
+        size: 5,
       },
     })
     .then(({ data }) => {
       return data.content;
     });
 
-const useCommunityShortsQuery = (page: number, size: number) => {
-  return useQuery([queryKeys.Shorts], () => fetcher(page, size));
+const useCommunityShortsQuery = () => {
+  return useQuery([queryKeys.Shorts], fetcher, {
+    enabled: false
+  });
 };
 
 export default useCommunityShortsQuery;
