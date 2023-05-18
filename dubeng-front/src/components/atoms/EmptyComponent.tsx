c973ba7f1d74445878d7bbc2763fa701a1365778
eondo <vov06144@naver.com>
2023-05-18 09:42:17 +0900

    feat: change css style

--- dubeng-front/src/components/atoms/EmptyComponent.tsx
+++ dubeng-front/src/components/atoms/EmptyComponent.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import EmptyImage from "../../../public/images/logo/EmptyImage.png";
 import { EmptyType } from "../../enum/statusType";
+import CommonButton from "./CommonButton";
+import DirectLoginButton from "./DirectLoginButton";
+import Link from "next/link";
 
 interface Iprops {
   status: string;
@@ -50,6 +53,18 @@ export default function EmptyComponent({ status }: Iprops) {
         <p className="mt-10 text-14 font-semibold text-dubgray">
           오늘은 더 이상 투표할 수 없습니다
         </p>
+      </div>
+    );
+  } else if (status === EmptyType.NO_RECOMMEND) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <Image src={EmptyImage} alt="EmptyImage" />
+        <p className="mt-10 text-14 font-semibold text-dubgray">
+          로그인하여 맞춤형 영상을 추천 받아보세요!
+        </p>
+        <Link href={"/login"}>
+          <DirectLoginButton children={"지금 바로 로그인하기"} />
+        </Link>
       </div>
     );
   }
