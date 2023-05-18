376f45b4ee72e6d744cf84563e96714bb79ae8c2
최정온 <chjo0330@gmail.com>
2023-05-19 01:40:07 +0900

    feat: update Background

--- dubeng-front/src/components/atoms/Background.tsx
+++ dubeng-front/src/components/atoms/Background.tsx
@@ -5,7 +5,7 @@ interface Iprops {
 export default function Background({ onClick }: Iprops) {
   return (
     <div
-      className="fixed top-[50%] left-[50%] z-25 h-full min-h-full w-full min-w-full translate-x-[-50%] translate-y-[-50%] bg-dubblack bg-opacity-50"
+      className="fixed top-[50%] left-[50%] z-50 h-full min-h-full w-full min-w-full translate-x-[-50%] translate-y-[-50%] bg-dubblack bg-opacity-50"
       onClick={onClick}
     ></div>
   );
