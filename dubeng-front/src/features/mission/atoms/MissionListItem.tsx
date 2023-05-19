682a5648a7f996a69a1389063315974fbdcb3b2d
eondo <vov06144@naver.com>
2023-05-19 09:21:38 +0900

    fix: fix Link closing tag missing error

--- dubeng-front/src/features/mission/atoms/MissionListItem.tsx
+++ dubeng-front/src/features/mission/atoms/MissionListItem.tsx
@@ -33,37 +33,39 @@ export default function MissionListItem({
     );
   } else {
     return (
-      <Link href={`/dubbing/${videoId}`}>
-        <div className="w-150 h-201 relative rounded-tl-lg bg-[#F8F8F8] rounded-lg mx-12 mb-8">
-          <div className={getBoxStyle(color)}>
-            <div className="w-80 h-80 rounded-lg">
-              <img
-                src={`/assets/${assetUrl}.PNG`}
-                alt={"objectImage"}
-                width={80}
-                height={80}
-                className="rounded-lg w-80 h-80"
-              />
+      <div>
+        <Link href={`/dubbing/${videoId}`}>
+          <div className="w-150 h-201 relative rounded-tl-lg bg-[#F8F8F8] rounded-lg mx-12 mb-8">
+            <div className={getBoxStyle(color)}>
+              <div className="w-80 h-80 rounded-lg">
+                <img
+                  src={`/assets/${assetUrl}.PNG`}
+                  alt={"objectImage"}
+                  width={80}
+                  height={80}
+                  className="rounded-lg w-80 h-80"
+                />
+              </div>
+              <div className="bg-black opacity-50 absolute top-0 left-0 w-150 h-150 rounded-t-lg"></div>
+              <div className="absolute flex flex-col items-center justify-center">
+                <MdOutlineLock size={32} className=" text-dubgraylight" />
+                <p className=" text-dubgraylight text-12">
+                  더빙하고 아이템 얻기!
+                </p>
+              </div>
             </div>
-            <div className="bg-black opacity-50 absolute top-0 left-0 w-150 h-150 rounded-t-lg"></div>
-            <div className="absolute flex flex-col items-center justify-center">
-              <MdOutlineLock size={32} className=" text-dubgraylight" />
-              <p className=" text-dubgraylight text-12">
-                더빙하고 아이템 얻기!
+            <div className="w-150 h-51 flex justify-center items-center rounded-b-lg bg-dubgraylight border-1 border-dubgraydeep">
+              <p className="text-14 font-bold text-dubblack text-center break-keep">
+                {title}
               </p>
             </div>
           </div>
-          <div className="w-150 h-51 flex justify-center items-center rounded-b-lg bg-dubgraylight border-1 border-dubgraydeep">
+          <div className="w-150 h-51 flex justify-center items-center rounded-b-lg bg-white border-1 border-dubgraymedium">
             <p className="text-14 font-bold text-dubblack text-center break-keep">
               {title}
             </p>
           </div>
-        </div>
-        <div className="w-150 h-51 flex justify-center items-center rounded-b-lg bg-white border-1 border-dubgraymedium">
-          <p className="text-14 font-bold text-dubblack text-center break-keep">
-            {title}
-          </p>
-        </div>
+        </Link>
       </div>
     );
   }
