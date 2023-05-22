3da9e4f14e5b1960cd5faeeae92a9185b658bb93
최정온 <chjo0330@gmail.com>
2023-05-22 21:21:56 +0900

    hotfix: shorts script add

--- dubeng-front/src/pages/community/shorts/index.tsx
+++ dubeng-front/src/pages/community/shorts/index.tsx
@@ -177,7 +177,7 @@ export default function ShortsProductPage() {
                             isScrap={false}
                           />
                         </div>
-                        {/* <div className="h-200 pt-32 overflow-y-scroll scrollbar-hide bg-black container mx-auto mb-16 w-391 mt-15">
+                        <div className="h-200 pt-32 overflow-y-scroll scrollbar-hide bg-black container mx-auto mb-16 w-391 mt-15">
                           {content.scriptList &&
                             content.scriptList.map(
                               (item: any, index: number) => {
@@ -196,7 +196,7 @@ export default function ShortsProductPage() {
                                 );
                               }
                             )}
-                        </div> */}
+                        </div>
                       </>
                     </div>
                   </div>
