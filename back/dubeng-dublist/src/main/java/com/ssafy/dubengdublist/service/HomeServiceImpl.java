9ed9ed8c58f6e7521478418a39f3608e33e3b504
김지희 <cjg05034@naver.com>
2023-06-14 23:15:22 +0900

    feat: modify dubEng logic to use Redis

--- back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/service/HomeServiceImpl.java
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/service/HomeServiceImpl.java
@@ -3,17 +3,24 @@ package com.ssafy.dubengdublist.service;
 import com.ssafy.dubengdublist.dto.home.HomeDubKingRes;
 import com.ssafy.dubengdublist.dto.home.HomePopularityRes;
 import com.ssafy.dubengdublist.dto.home.HomeRankRes;
+import com.ssafy.dubengdublist.entity.User;
+import com.ssafy.dubengdublist.exception.NotFoundException;
+import com.ssafy.dubengdublist.repository.UserRepository;
 import com.ssafy.dubengdublist.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class HomeServiceImpl implements HomeService{
 
     private final VideoRepository videoRepository;
+    private final UserRepository userRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+
 
     public List<HomePopularityRes> findHomePopularity(){
         return videoRepository.findAllHomePopularity();
@@ -24,7 +31,25 @@ public class HomeServiceImpl implements HomeService{
     }
 
     public List<HomeDubKingRes> findHomeDubKing(){
-        return videoRepository.findHomeDubKing();
+        List<HomeDubKingRes> result = new ArrayList<>();
+        Set<String> redisKeys = redisTemplate.keys("dubKingTop3");
+        Iterator<String> it = redisKeys.iterator();
+        Long i = 0L;
+        while (it.hasNext()) {
+            if(result.size()>3) break;
+
+            String data = it.next();
+            String userId = data.split("::")[0];
+            Long voteCnt = Long.parseLong(data.split("::")[1]);
+            Optional<User> ouser = userRepository.findById(userId);
+            if(!ouser.isPresent()){
+                continue; // redis에는 있었는데 DB에는 없는 경우
+            }
+            User user = ouser.get();
+            result.add(new HomeDubKingRes(i++, userId, user.getNickname(), user.getProfileImage(), voteCnt));
+        }
+
+        return result;
     }
 
 }
--- back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/service/RedisService.java
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/service/RedisService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,23 +32,22 @@ public class RedisService {
 
     @Scheduled(fixedDelay = 300000, initialDelay = 1000)
     @Transactional
-    public void addPlayCountFromRedis(){
+    public void addPlayCountFromRedis() {
 
         Set<String> redisKeys = redisTemplate.keys("recordPlayCnt*");
         Iterator<String> it = redisKeys.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String data = it.next();
             Long recordId = Long.parseLong(data.split("::")[1]);
             Object ovalue = redisTemplate.opsForValue().get(data);
             Long playCnt = 0L;
-            if(ovalue!=null)
-                playCnt = Long.parseLong((String)ovalue);
+            if (ovalue != null)
+                playCnt = Long.parseLong((String) ovalue);
 
             Optional<Record> orecord = recordRepository.findById(recordId);
-            if(!orecord.isPresent()){
+            if (!orecord.isPresent()) {
                 redisTemplate.delete(data);
-            }
-            else {
+            } else {
                 recordRepository.updatePlayCount(recordId, playCnt);
             }
         }
@@ -55,31 +55,30 @@ public class RedisService {
 
     @Scheduled(fixedDelay = 3600000, initialDelay = 15000)
     @Transactional
-    public void updateLikeFromRedis(){
+    public void updateLikeFromRedis() {
         recordLikeRepository.deleteAll();
         recordLikeRepository.flush();
 
         Set<String> redisKeys = redisTemplate.keys("like_userId*");
         Iterator<String> it = redisKeys.iterator();
         List<RecordLike> likeList = new ArrayList<>();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String data = it.next();
             String userId = data.split("::")[1]; // 유저 아이디들
             Set<Object> records = redisTemplate.opsForSet().members(data);
 
             Optional<User> ouser = userRepository.findById(userId);
-            if(!ouser.isPresent()){
+            if (!ouser.isPresent()) {
                 redisTemplate.delete(data);
             }
             User user = ouser.get();
-            if(records.isEmpty()) return;
-            for(Object o : records){
+            if (records.isEmpty()) return;
+            for (Object o : records) {
                 Long recordId = Long.parseLong((String) o);
                 Optional<Record> orecord = recordRepository.findById(recordId);
-                if(!orecord.isPresent()){
+                if (!orecord.isPresent()) {
                     redisTemplate.opsForSet().remove(data, o);
-                }
-                else {
+                } else {
                     Record record1 = orecord.get();
                     likeList.add(new RecordLike(user, record1, true));
                 }
@@ -87,25 +86,25 @@ public class RedisService {
         }
         recordLikeRepository.saveAll(likeList);
     }
+
     @Scheduled(fixedDelay = 3600000, initialDelay = 240000)
     @Transactional
-    public void updateScrapFromRedis(){
+    public void updateScrapFromRedis() {
         videoBookmarkRepository.deleteAll();
         videoBookmarkRepository.flush();
         em.flush();
         em.clear();
         Set<String> redisKeys = redisTemplate.keys("scrap_userId*");
         Iterator<String> it = redisKeys.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String data = it.next();
             String userId = data.split("::")[1]; // 유저 아이디들
             Set<Object> videos = redisTemplate.opsForSet().members(data);
 
             Optional<User> ouser = userRepository.findById(userId);
-            if(!ouser.isPresent()){
+            if (!ouser.isPresent()) {
                 redisTemplate.delete(data);
-            }
-            else {
+            } else {
                 User user = ouser.get();
                 for (Object ovideo : videos) {
                     Long videoId = Long.parseLong((String) ovideo);
@@ -121,32 +120,57 @@ public class RedisService {
     }
 
     @Scheduled(cron = "0 0 0 * * *")
-    public void initVoteCount(){
+    public void initVoteCount() {
         Set<String> voteCntKeys = redisTemplate.keys("vote_userId*");
         redisTemplate.delete(voteCntKeys);
     }
 
     //    @Scheduled(cron = "30 0 0 * * 1") // 매주 일요일 밤 자정 + 30초 갱신
     @Scheduled(cron = "30 0 0 * * *") // 모니터링용 initVoteCount랑 동시 실행 방지로 30초에 작동
-    public void updateDubKing(){
+    public void updateDubKing() {
+        PriorityQueue<dubKingVote> priorityDubkingVotes = new PriorityQueue<>();
 
         dubKingRepository.deleteAll();
         dubKingRepository.flush();
         Set<String> redisKeys = redisTemplate.keys("dubKing_userId*");
+
+        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+        String dubKingKey = "dubKingTop3";
+        redisTemplate.delete(dubKingKey);  // 이전에 들어 있던 거 일단 지움.
+
         Iterator<String> it = redisKeys.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             String data = it.next();
             String userId = data.split("::")[1];
             Long voteCnt = Long.parseLong((String) redisTemplate.opsForValue().get(data));
             Optional<User> ouser = userRepository.findById(userId);
-            if(!ouser.isPresent()){
+            if (!ouser.isPresent()) {
                 redisTemplate.delete(data); // 해당 user 없으면 레디스에서 지움.
+            } else {
+                priorityDubkingVotes.add(new dubKingVote(userId, voteCnt));
             }
-            else {
-                User user = ouser.get();
-                dubKingRepository.save(new DubKing(user, voteCnt, true));
-            }
+        }
+        // pQ에 있는거 top3만 넣을거야
+        for (int i = 0; i < 8; i++) {
+            if(priorityDubkingVotes.isEmpty()) break;
+            dubKingVote cur = priorityDubkingVotes.poll();
+            String value = cur.userId+"::"+cur.voteCnt;
+            setOperations.add(dubKingKey,value );
         }
     }
 
+    static class dubKingVote implements Comparable<dubKingVote> {
+        String userId;
+        Long voteCnt;
+        dubKingVote(String userId, Long voteCnt) {
+            this.userId = userId;
+            this.voteCnt = voteCnt;
+        }
+        @Override
+        public int compareTo(dubKingVote o) {
+            if (o.voteCnt - this.voteCnt >= 0L) {
+                return 1;
+            } else return 0;
+        }
+    }
 }
