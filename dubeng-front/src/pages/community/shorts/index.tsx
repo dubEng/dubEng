6f830e9ad87c1f01f2f092943d84c1b2d38ebf60
김도언 <vov06144@naver.com>
2023-06-26 23:58:38 +0900

    feat: add shortspage logic

--- dubeng-front/src/pages/community/shorts/index.tsx
+++ dubeng-front/src/pages/community/shorts/index.tsx
@@ -1,86 +1,33 @@
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
-import YouTube, { YouTubePlayer, YouTubeProps } from "react-youtube";
-
-import Image from "next/image";
-import ShortsTitle from "@/features/community/molecules/ShortsTitle";
-import DefaultImage from "../../../../public/images/default/mic_profile.png";
-import usePlayCountUpQuery from "@/apis/community/queries/usePlayCountUpQuery";
-import { useSelector } from "react-redux";
-import { RootState } from "@/stores/store";
-
-import ReactFullpage from "@fullpage/react-fullpage";
 import useCommunityShortsQuery from "@/apis/community/queries/useCommunityShortsQuery";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { FullPage, Slide } from "react-full-page";
+import Image from "next/image";
 
-type Credits = {
-  enabled?: boolean;
-  label?: string;
-  position?: "left" | "right";
-};
+import DefaultImage from "../../../../public/images/default/mic_profile.png";
+import YouTube, { YouTubePlayer, YouTubeProps } from "react-youtube";
+import ShortsTitle from "@/features/community/molecules/ShortsTitle";
 
-const pluginWrapper = () => {
-  /*
-   * require('../static/fullpage.scrollHorizontally.min.js'); // Optional. Required when using the "scrollHorizontally" extension.
-   */
-};
-
-export default function ShortsProductPage() {
-  const router = useRouter();
-
-  const userId = useSelector((state: RootState) => state.user.userId);
-
-  const [isPlayed, setIsPlayed] = useState<null | boolean>(null);
-
+export default function ShortsPage() {
   const { data: contentList, refetch } = useCommunityShortsQuery();
 
   const [youtubePlayer, setYoutubePlayer] = useState<YouTubePlayer>();
+
+  // const [youtubePlayerList, setYoutubePlayerList] = useState<YouTubePlayer[]>(
+  //   []
+  // );
 
   const [audioPath, setAudioPath] = useState<string>("");
 
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  function transferYoutube(videoPath: string) {
-    const originalUrl = videoPath;
-    const splitUrl = originalUrl.split("watch?v=");
-    const transferVideoPath = splitUrl[1];
-    return transferVideoPath;
-  }
+  const [isPlayed, setIsPlayed] = useState<null | boolean>(null);
 
-  // player 준비시 실행
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    const player = event.target;
-    setYoutubePlayer(player);
-    player.pauseVideo();
-    player.mute();
-  };
+  const [currentPage, setCurrentPage] = useState<number>(0);
 
-  // const { data: playCountUp, refetch } = usePlayCountUpQuery(
-  //   userId,
-  //   Number(router.query.id)
-  // );
+  console.log("data", contentList);
 
-  const onPlay: YouTubeProps["onPlay"] = (event) => {
-    // console.log("onPlay");
-    // console.log("event", event);
-  };
-
-  const onStateChange: YouTubeProps["onStateChange"] = (event) => {
-    // console.log("onStateChange");
-
-    if (event.data === 1) {
-      if (audioRef.current) {
-        audioRef.current.play();
-      }
-      setIsPlayed(true);
-      // 재생 중일 때
-    } else if (event.data === 2) {
-      if (audioRef.current) {
-        audioRef.current.pause();
-      }
-      //일시 정지 시
-    }
-  };
+  // 첫 렌더링에만 쇼츠콘텐츠 get 해오기
   useEffect(() => {
     refetch();
   }, []);
@@ -89,126 +36,267 @@ export default function ShortsProductPage() {
     if (contentList) {
       setAudioPath(contentList[0].recordPath);
     }
+    // for (let i=0;i<contentList.length;i++) {
+    //   setYoutubePlayerList(...youtubePlayerList, youtubePlayer)
+    // }
   }, [contentList]);
 
-  const onLeave = (origin: any, destination: any, direction: any) => {
-    // console.log("onLeave", { origin, destination, direction });
-    // console.log("destination index", destination.index);
-    // console.log("recordPath", contentList[destination.index].recordPath);
+  // 유튜브 고유key값으로 가져오는 함수
+  function processVideoPath(videoPath: string) {
+    const originalUrl = videoPath;
+    const splitUrl = originalUrl.split("watch?v=");
+    const processedVideoPath = splitUrl[1];
+    return processedVideoPath;
+  }
 
-    setAudioPath(contentList[destination.index].recordPath);
+  // 음원 재생 관련
+  // player 준비시 실행
+  // const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+  //   console.log("onPlayerReady 안");
+  //   console.log("🔷", event.target.videoTitle);
+  //   const player = event.target;
+  //   setYoutubePlayer(player);
+  //   player.pauseVideo();
+  //   player.mute();
+  // };
+
+  const onPlay: YouTubeProps["onPlay"] = (event) => {
+    setYoutubePlayer(event.target);
+    // event.target.seekTo(contentList[currentPage].startTime);
   };
 
-  const credits: Credits = {
-    enabled: false,
-    label: "my custom",
-    position: "left",
+  const onStateChange: YouTubeProps["onStateChange"] = (event) => {
+    console.log("👤onStateChange");
+    event.target.mute();
+    // player.mute()
+
+    if (event.data === 1) {
+      // event.target.seekTo(contentList[currentPage].startTime);
+      if (audioRef.current) {
+        audioRef.current.play();
+      }
+      setIsPlayed(true);
+    } else if (event.data === 2) {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+    }
+  };
+
+  const afterChange = (event: any) => {
+    setAudioPath(contentList[event.to].recordPath);
+    // console.log("플레이어", youtubePlayer);
+    setCurrentPage(event.to);
+  };
+
+  const beforeChange = (event: any) => {
+    if (youtubePlayer) {
+      console.log(
+        "bC발생하고 인덱스",
+        "지금 재생될 startTime",
+        contentList[event.from].startTime
+      );
+      youtubePlayer.pauseVideo();
+      youtubePlayer.seekTo(contentList[event.from].startTime);
+      console.log("어디서 왔느냐", youtubePlayer.videoTitle);
+    }
   };
 
   return (
     <>
       {contentList && (
-        <ReactFullpage
-          licenseKey={"OPEN-SOURCE-GPLV3-LICENSE"}
-          navigation={false}
-          onLeave={onLeave}
-          pluginWrapper={pluginWrapper}
-          debug={false}
-          scrollOverflow={false}
-          credits={credits}
-          render={(comp: any) => (
-            <ReactFullpage.Wrapper>
-              {contentList.map((content: any, index: number) => {
-                return (
-                  <div key={content.recordId} className="section">
-                    <div className="w-full h-screen bg-black flex flex-col items-center justify-start">
-                      <>
-                        <Link href={`/mypage/${content.userId}`}>
-                          <div className="flex flex-row mt-16 mb-16 items-center w-390 px-16">
-                            <Image
-                              src={content.profileImage ?? DefaultImage}
-                              alt="profileImage"
-                              width={24}
-                              height={24}
-                              className="rounded-full"
-                            />
-                            <p className="ml-4 text-dubgraymedium">
-                              {content.nickname}
-                            </p>
-                          </div>
-                        </Link>
-                        <YouTube
-                          videoId={transferYoutube(content.videoPath)}
-                          opts={{
-                            height: "221",
-                            width: "390",
-                            playerVars: {
-                              start: content.startTime,
-                              end: content.endTime,
-                              autoplay: 0,
-                              modestbranding: 0, //컨트롤 바에 youtube 로고를 표시하지 않음
-                              controls: 0,
-                            },
-                          }}
-                          onReady={onPlayerReady}
-                          onPlay={onPlay}
-                          onStateChange={onStateChange}
-                          onEnd={(e) => {
-                            youtubePlayer.pauseVideo();
-                            youtubePlayer.seekTo(content.startTime);
-
-                            if (audioRef.current) {
-                              audioRef.current.pause();
-                              audioRef.current.currentTime = 0;
-                            }
-                          }}
+        <FullPage
+          controls
+          controlProps
+          scrollMode={"full-page"}
+          afterChange={afterChange}
+          beforeChange={beforeChange}
+        >
+          {contentList.map((content: any, index: number) => {
+            return (
+              <Slide key={content.recordId}>
+                <div className="w-full h-screen bg-black flex flex-col items-center justify-start">
+                  <>
+                    <Link href={`/mypage/${content.userId}`}>
+                      <div className="flex flex-row mt-16 mb-16 items-center w-390 px-16">
+                        <Image
+                          src={content.profileImage ?? DefaultImage}
+                          alt="profileImage"
+                          width={24}
+                          height={24}
+                          className="rounded-full"
                         />
-                        <audio
-                          ref={audioRef}
-                          style={{ display: "none" }}
-                          src={audioPath}
-                        />
-                        <div className="mt-16">
-                          <ShortsTitle
-                            userId={content.userId}
-                            title={content.title}
-                            createdDate={content.createdDate}
-                            recordCommentCount={content.recordCommentCount}
-                            isScrap={false}
-                          />
-                        </div>
-                        <div className="relative">
-                          <div className="h-200 pt-32 overflow-y-scroll scrollbar-hide bg-black container mx-auto mb-16 w-391 mt-15">
-                            {content.scriptList &&
-                              content.scriptList.map(
-                                (item: any, index: number) => {
-                                  return (
-                                    <div
-                                      className={`script-element-${index} mb-8 mx-20 flex flex-col items-center`}
-                                      key={index}
-                                    >
-                                      <p className="text-16 text-white">
-                                        {item.content}
-                                      </p>
-                                      <p className="text-14 text-[#8E8D8D]">
-                                        {item.translateContent}
-                                      </p>
-                                    </div>
-                                  );
-                                }
-                              )}
-                          </div>
-                          <div className="opacity-100 absolute top-0 left-0 w-391 h-200"></div>
-                        </div>
-                      </>
+                        <p className="ml-4 text-dubgraymedium">
+                          {content.nickname}
+                        </p>
+                      </div>
+                    </Link>
+                    <YouTube
+                      videoId={processVideoPath(content.videoPath)}
+                      opts={{
+                        height: "221",
+                        width: "390",
+                        playerVars: {
+                          start: content.startTime,
+                          end: content.endTime,
+                          autoplay: 0,
+                          modestbranding: 0, //컨트롤 바에 youtube 로고를 표시하지 않음
+                          controls: 0,
+                        },
+                      }}
+                      // onReady={onPlayerReady}
+                      onPlay={onPlay}
+                      onStateChange={onStateChange}
+                      onEnd={(e) => {
+                        // youtubePlayer.pauseVideo();
+                        // youtubePlayer.seekTo(content.startTime);
+                        // if (audioRef.current) {
+                        //   audioRef.current.pause();
+                        //   audioRef.current.currentTime = 0;
+                        // }
+                      }}
+                    />
+                    <audio
+                      controls={true}
+                      ref={audioRef}
+                      style={{ display: "none" }}
+                      src={audioPath}
+                    />
+                    <div className="mt-16">
+                      <ShortsTitle
+                        userId={content.userId}
+                        title={content.title}
+                        createdDate={content.createdDate}
+                        recordCommentCount={content.recordCommentCount}
+                        isScrap={false}
+                      />
                     </div>
-                  </div>
-                );
-              })}
-            </ReactFullpage.Wrapper>
-          )}
-        />
+                    <div className="relative">
+                      <div className="h-200 pt-32 overflow-y-scroll scrollbar-hide bg-black container mx-auto mb-16 w-391 mt-15">
+                        {content.scriptList &&
+                          content.scriptList.map((item: any, index: number) => {
+                            return (
+                              <div
+                                className={`script-element-${index} mb-8 mx-20 flex flex-col items-center`}
+                                key={index}
+                              >
+                                <p className="text-16 text-white">
+                                  {item.content}
+                                </p>
+                                <p className="text-14 text-[#8E8D8D]">
+                                  {item.translateContent}
+                                </p>
+                              </div>
+                            );
+                          })}
+                      </div>
+                      <div className="opacity-100 absolute top-0 left-0 w-391 h-200"></div>
+                    </div>
+                  </>
+                </div>
+              </Slide>
+            );
+          })}
+        </FullPage>
       )}
     </>
   );
 }
+
+// ex2
+// import React, { useState } from "react";
+// import { FullPage, Slide } from "react-full-page";
+
+// const controlsProps = {
+//   style: {
+//     left: "50%",
+//     paddingTop: "10px",
+//     position: "fixed",
+//     transform: "translateX(-50%)",
+//   },
+// };
+
+// const slides = [
+//   {
+//     color: "#2ECC40",
+//     content: 1,
+//   },
+//   {
+//     color: "#0074D9",
+//     content: 2,
+//   },
+//   {
+//     color: "#00c4ff",
+//     content: 3,
+//   },
+//   {
+//     color: "#d52685",
+//     content: 4,
+//   },
+// ];
+
+// const btnStyles = {
+//   position: "fixed",
+//   padding: "8px",
+// };
+
+// export default function FullPageExample() {
+//   const [visibleSlides, setVisibleSlides] = useState(slides);
+//   const onHideSlideClick = () => {
+//     if (visibleSlides.length === slides.length) {
+//       setVisibleSlides(slides.slice(0, -1));
+//       return;
+//     }
+//     setVisibleSlides(slides);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={onHideSlideClick} type="button" style={btnStyles}>
+//         toggle last slide
+//       </button>
+//       <FullPage controlsProps={controlsProps} scrollMode>
+//         {visibleSlides.map(({ color, content }) => (
+//           <Slide
+//             key={content}
+//             style={{
+//               background: color,
+//               display: "flex",
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             <h1>{content}</h1>
+//           </Slide>
+//         ))}
+//       </FullPage>
+//     </>
+//   );
+// }
+
+// EX 3
+// import { FullPage, Slide } from "react-full-page";
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// class TestApp extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <FullPage controls controlsProps={{ className: "slide-navigation" }}>
+//           <Slide>
+//             <div className="section-common section-area1">1</div>
+//           </Slide>
+//           <Slide>
+//             <div className="section-common section-area2">2</div>
+//           </Slide>
+//           <Slide>
+//             <div className="section-common section-area3">3</div>
+//           </Slide>
+//         </FullPage>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<TestApp />, document.getElementById("root"));
