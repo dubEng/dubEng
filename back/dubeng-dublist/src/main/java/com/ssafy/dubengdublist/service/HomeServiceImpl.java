33e7280da0b87b6e3d9852683c534b8a440d60b5
김지희 <cjg05034@naver.com>
2023-07-05 18:06:14 +0900

    feat: modify dubKing top service to use sotred set

--- back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/service/HomeServiceImpl.java
+++ back/dubeng-dublist/src/main/java/com/ssafy/dubengdublist/service/HomeServiceImpl.java
@@ -9,47 +9,46 @@ import com.ssafy.dubengdublist.repository.UserRepository;
 import com.ssafy.dubengdublist.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class HomeServiceImpl implements HomeService{
+public class HomeServiceImpl implements HomeService {
 
     private final VideoRepository videoRepository;
     private final UserRepository userRepository;
     private final RedisTemplate<String, Object> redisTemplate;
 
 
-    public List<HomePopularityRes> findHomePopularity(){
+    public List<HomePopularityRes> findHomePopularity() {
         return videoRepository.findAllHomePopularity();
     }
 
-    public List<HomeRankRes> findHomeRank(){
+    public List<HomeRankRes> findHomeRank() {
         return videoRepository.findAllHomeRank();
     }
 
-    public List<HomeDubKingRes> findHomeDubKing(){
+    public List<HomeDubKingRes> findHomeDubKing() {
         List<HomeDubKingRes> result = new ArrayList<>();
         Set<String> redisKeys = redisTemplate.keys("dubKingTop3");
         Iterator<String> it = redisKeys.iterator();
         Long i = 0L;
-        Set<Object> users = redisTemplate.opsForSet().members("dubKingTop3");
-
-        for(Object o : users){
-            String data = o.toString();
-            String userId = data.split("::")[0];
-            Long voteCnt = Long.parseLong(data.split("::")[1]);
+        ZSetOperations<String, Object> ZSetOperations = redisTemplate.opsForZSet();
+        Set<ZSetOperations.TypedTuple<Object>> typedTuples = ZSetOperations.reverseRangeWithScores("dubkingTop3", 0, 4);
+        for (ZSetOperations.TypedTuple<Object> t : typedTuples) {
+            String userId = (String)t.getValue();
             Optional<User> ouser = userRepository.findById(userId);
-            if(!ouser.isPresent()){
+            if (!ouser.isPresent()) {
                 continue; // redis에는 있었는데 DB에는 없는 경우
             }
             User user = ouser.get();
+            Long voteCnt = t.getScore().longValue();
             result.add(new HomeDubKingRes(i++, userId, user.getNickname(), user.getProfileImage(), voteCnt));
         }
-
-        result.sort(HomeDubKingRes::compareTo);
         return result;
 
     }
