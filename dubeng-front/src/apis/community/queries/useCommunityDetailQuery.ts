94f019a36d48091eb292a0576737618ba2e739cb
최정온 <chjo0330@gmail.com>
2023-07-12 21:19:02 +0900

    fix: browser cash busting

--- dubeng-front/src/apis/community/queries/useCommunityDetailQuery.ts
+++ dubeng-front/src/apis/community/queries/useCommunityDetailQuery.ts
@@ -5,7 +5,8 @@ import * as queryKeys from "@/constants/queryKeys";
 const fetcher = (recordId: string) =>
   axios
     .get(
-      process.env.NEXT_PUBLIC_BASE_URL + `/dub/community/detail/${recordId}`,
+      process.env.NEXT_PUBLIC_BASE_URL +
+        `/dub/community/detail/${recordId}?timestamp=${Date.now()}`,
       {
         headers: {
           "Cache-Control": "no-cache",
