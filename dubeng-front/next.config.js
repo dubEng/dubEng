34c1def0a4eebc7d263710990cd34e45d2708060
김도언 <vov06144@naver.com>
2023-09-22 21:15:25 +0900

    hotfix: add removeConsole config

--- dubeng-front/next.config.js
+++ dubeng-front/next.config.js
@@ -1,19 +1,28 @@
-const withPlugins = require('next-compose-plugins');
+const withPlugins = require("next-compose-plugins");
 
 const withTwin = require("./withTwin.js");
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = withTwin({
   reactStrictMode: false,
+  compiler: {
+    removeConsole: {
+      exclude: ["error", "warn"],
+    },
+  },
   images: {
-    domains: ["i.ytimg.com", "k.kakaocdn.net", "dubingdubing.s3.ap-northeast-2.amazonaws.com", "lh3.googleusercontent.com"]
+    domains: [
+      "i.ytimg.com",
+      "k.kakaocdn.net",
+      "dubingdubing.s3.ap-northeast-2.amazonaws.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 });
 
 module.exports = withPlugins([withTwin, withPWA], nextConfig);
-
